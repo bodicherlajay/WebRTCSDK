c6fefc1976c82f05959971ccd0c6747b7c805e1f
danebert <debert@solutionsiq.com>
2014-03-12 13:52:30 -0700

    Reformatting the code (replace tabs with spaces)

--- js/restClient.js
+++ js/restClient.js
@@ -1,69 +1,71 @@
-var RESTClient = (function () {
+var RESTClient = (function() {
 
-  function RESTClient (config) {
-    config = config || {};
-    
-    // default ajax configuration
-    config.async =        config.async || true;
-    config.timeout =      config.timeout || 10000;
-    config.success =      config.success || function () {};     
-    config.error =        config.error || function () {};
-  }
-  
-  // private methods
+    function RESTClient(config) {
+        config = config || {};
 
-  function success (successCallback) {
-    successCallback.call(this, JSON.parse(this.responseText));
-  }
+        // default ajax configuration
+        config.async = config.async || true;
+        config.timeout = config.timeout || 10000;
+        config.success = config.success || function() {
+        };
+        config.error = config.error || function() {
+        };
+    }
 
-  function error (errorCallback) {
-    errorCallback.call(this, this.responseText);
-  }
-  
-  // public methods
-  RESTClient.prototype.ajax = function (config) {
+    // private methods
 
-    config.headers = config.headers || {};
-    config.headers['Content-Type'] = config.headers['Content-Type'] || 'application/json';
+    function success(successCallback) {
+        successCallback.call(this, JSON.parse(this.responseText));
+    }
 
-    var xhr =  new XMLHttpRequest(),
-        data = config.data && JSON.stringify(config.data);
+    function error(errorCallback) {
+        errorCallback.call(this, this.responseText);
+    }
 
-      xhr.timeout = config.timeout;
+    // public methods
+    RESTClient.prototype.ajax = function(config) {
 
-      // success callback
-      xhr.onload = success.bind(xhr, config.success);
+        config.headers = config.headers || {};
+        config.headers['Content-Type'] = config.headers['Content-Type'] || 'application/json';
 
-      // error callback
-      xhr.onerror = error.bind(xhr, config.error);
+        var xhr = new XMLHttpRequest(),
+                data = config.data && JSON.stringify(config.data);
 
-      // open connection
-      xhr.open(config.method, config.url, config.async);
- 
-      // optional headers from config
-      for (var key in config.headers) {
-        if (config.headers.hasOwnProperty(key)) {
-          xhr.setRequestHeader(key, config.headers[key]);
+        xhr.timeout = config.timeout;
+
+        // success callback
+        xhr.onload = success.bind(xhr, config.success);
+
+        // error callback
+        xhr.onerror = error.bind(xhr, config.error);
+
+        // open connection
+        xhr.open(config.method, config.url, config.async);
+
+        // optional headers from config
+        for (var key in config.headers) {
+            if (config.headers.hasOwnProperty(key)) {
+                xhr.setRequestHeader(key, config.headers[key]);
+            }
         }
-      }
 
-      xhr.send(data);   
-  };
-    
-  RESTClient.prototype.get = function (config) {
-    config.method = 'get';
-    this.ajax(config);
-  };
+        xhr.send(data);
+    };
 
-  RESTClient.prototype.post = function (config) {
-    config.method = 'post';
-    this.ajax(config);
-  };
+    RESTClient.prototype.get = function(config) {
+        config.method = 'get';
+        this.ajax(config);
+    };
 
-  RESTClient.prototype.delete = function (config) {
-    config.method = 'delete';
-    this.ajax(config);
-  };
+    RESTClient.prototype.post = function(config) {
+        config.method = 'post';
+        this.ajax(config);
+    };
 
-  return RESTClient;
+    RESTClient.prototype.delete = function(config) {
+        config.method = 'delete';
+        this.ajax(config);
+    };
+
+    return RESTClient;
 })();
--- test/restClientTest.js
+++ test/restClientTest.js
@@ -1,181 +1,181 @@
-describe('Rest Client', function () {
+describe('Rest Client', function() {
 
-	// globals used in each test.
-	var errorSpy,
-		successSpy,
-		config,
-		rc,
-		requests;
+    // globals used in each test.
+    var errorSpy,
+            successSpy,
+            config,
+            rc,
+            requests;
 
-	beforeEach(function() {
+    beforeEach(function() {
 
-		rc = new RESTClient();
+        rc = new RESTClient();
 
- 		this.xhr = sinon.useFakeXMLHttpRequest();
+        this.xhr = sinon.useFakeXMLHttpRequest();
         requests = this.requests = [];
 
-        this.xhr.onCreate = function (xhr) {
+        this.xhr.onCreate = function(xhr) {
             requests.push(xhr);
         };
-		
-		successSpy = sinon.spy();
-		errorSpy = sinon.spy();
 
-		config = {
-			url: 'url',
-			data: {some: 'data'},
-			success: successSpy,
-			error: errorSpy,
-			async: true
-		}
-	
-	});
+        successSpy = sinon.spy();
+        errorSpy = sinon.spy();
 
-	afterEach(function () {
-		rc = null;
-		successSpy = null;
-		errorSpy = null;
-		config = null;
-		this.xhr.restore();
-	});
+        config = {
+            url: 'url',
+            data: {some: 'data'},
+            success: successSpy,
+            error: errorSpy,
+            async: true
+        }
 
-	it('should create a new RESTClient', function () {
-		expect(rc).to.exist;
-		expect(rc.get).to.be.a('function');
-	});
-
-	it('should call xhr open correctly for get', function () {
-		//act
-		rc.get(config);
-		
-		//assert
-		expect(this.requests[0].url).to.equal(config.url);
-		expect(this.requests[0].method).to.equal('get');
-		expect(this.requests[0].async).to.equal(config.async);
-	});
-
-	it('should call xhr open correctly for post', function () {
-		//act
-		rc.post(config);
-		
-		//assert
-		expect(this.requests[0].url).to.equal(config.url);
-		expect(this.requests[0].method).to.equal('post');
-		expect(this.requests[0].async).to.equal(config.async);
-	});
-
-	it('should call xhr open correctly for delete', function () {
-		//act
-		rc.delete(config);
-		
-		//assert
-		expect(this.requests[0].url).to.equal(config.url);
-		expect(this.requests[0].method).to.equal('delete');
-		expect(this.requests[0].async).to.equal(config.async);
-	});
-
-	it('should call xhr send with serialized data string', function () {
-		//act
-		rc.post(config);
-
-		//assert
-		expect(this.requests[0].requestBody).to.equal('{"some":"data"}');
-	});
-
-	it('should call success callback', function () {
-		//act 
-		rc.get(config);
-
- 		this.requests[0].respond(200, { "Content-Type": "application/json" },'{"some":"data"}');
-
-		//assert
-		var calledWith = successSpy.getCall(0).args[0];
-		expect(calledWith.some).to.equal('data');
-		sinon.assert.notCalled(errorSpy);
-	});
-
-	// Not sure why this isn't working ... the sinon framework is behaving as if '500' is successful.
-	xit('should call error callback', function () {
-	
-		//act
-		rc.get(config);
-
-	    this.requests[0].respond(500, {"Content-Type": "text/plain"}, '{"some":"data"}');
-
-		//assert
-		sinon.assert.called(errorSpy);
-		expect(this.requests[0].status).to.equal(500);
-		var calledWith = errorSpy.getCall(0).args[0];
-		expect(calledWith.some).to.equal('data');
-		sinon.assert.notCalled(successSpy);
-	});
-
-	it('should only set Content-Type header if none specified', function() {
-		//act
-		rc.get(config);
-		expect(this.requests[0].requestHeaders).to.eql({'Content-Type': 'application/json'});
-	});
-
-	it('should only set Content-Type header if {} specified', function() {
-		config.headers = {};
-
-		//act
-		rc.get(config);
-		expect(this.requests[0].requestHeaders).to.eql({'Content-Type': 'application/json'});
-	});
-
-	it('should set all headers specified', function() {
-		
- 		config.headers = {'one': 'oneValue',
- 						  'two': 'twoValue'};
- 
-		//act
-		rc.get(config);
-
-		expect(this.requests[0].requestHeaders['one']).to.equal('oneValue');
-		expect(this.requests[0].requestHeaders['two']).to.equal('twoValue');
-	});
-
-	it('should set content type header for post if no header specified', function(){
-		config.headers = null;
-
-		//act
-		rc.post(config);
-		expect(this.requests[0].requestHeaders['Content-Type']).to.contain('application/json');
-	});
-
-	it('should set content type header for post if not specified and retain existing headers', function(){
-		config.headers = {a:1};
-
-		//act
-		rc.post(config);
-		expect(this.requests[0].requestHeaders['Content-Type']).to.contain('application/json');
-		expect(this.requests[0].requestHeaders['a']).to.equal(1);
-	});
-
-	it('should not override Content-Type header if its specified', function(){
-		config.headers = {'Content-Type': 'application/xml'};
-
-		//act
-		rc.post(config);
-		expect(this.requests[0].requestHeaders['Content-Type']).to.contain('application/xml');
-	});
-
-	it('should use correct defaults if no config provided', function() {
-		var config = {};
-		var client = new RESTClient(config);
-		expect(config.success).to.be.a('function');
-		expect(config.error).to.be.a('function');
-		expect(config.timeout).to.equal(10000);
-		expect(config.async).to.be.true;
-	});
-
-	it('should use set the timeout on the XMLHttpRequest', function() {
-		config.timeout = 50;
-
-		//act
-		rc.ajax(config);
-   		expect(this.requests[0].timeout).to.equal(50);        
     });
-	
+
+    afterEach(function() {
+        rc = null;
+        successSpy = null;
+        errorSpy = null;
+        config = null;
+        this.xhr.restore();
+    });
+
+    it('should create a new RESTClient', function() {
+        expect(rc).to.exist;
+        expect(rc.get).to.be.a('function');
+    });
+
+    it('should call xhr open correctly for get', function() {
+        //act
+        rc.get(config);
+
+        //assert
+        expect(this.requests[0].url).to.equal(config.url);
+        expect(this.requests[0].method).to.equal('get');
+        expect(this.requests[0].async).to.equal(config.async);
+    });
+
+    it('should call xhr open correctly for post', function() {
+        //act
+        rc.post(config);
+
+        //assert
+        expect(this.requests[0].url).to.equal(config.url);
+        expect(this.requests[0].method).to.equal('post');
+        expect(this.requests[0].async).to.equal(config.async);
+    });
+
+    it('should call xhr open correctly for delete', function() {
+        //act
+        rc.delete(config);
+
+        //assert
+        expect(this.requests[0].url).to.equal(config.url);
+        expect(this.requests[0].method).to.equal('delete');
+        expect(this.requests[0].async).to.equal(config.async);
+    });
+
+    it('should call xhr send with serialized data string', function() {
+        //act
+        rc.post(config);
+
+        //assert
+        expect(this.requests[0].requestBody).to.equal('{"some":"data"}');
+    });
+
+    it('should call success callback', function() {
+        //act
+        rc.get(config);
+
+        this.requests[0].respond(200, {"Content-Type": "application/json"}, '{"some":"data"}');
+
+        //assert
+        var calledWith = successSpy.getCall(0).args[0];
+        expect(calledWith.some).to.equal('data');
+        sinon.assert.notCalled(errorSpy);
+    });
+
+    // Not sure why this isn't working ... the sinon framework is behaving as if '500' is successful.
+    xit('should call error callback', function() {
+
+        //act
+        rc.get(config);
+
+        this.requests[0].respond(500, {"Content-Type": "text/plain"}, '{"some":"data"}');
+
+        //assert
+        sinon.assert.called(errorSpy);
+        expect(this.requests[0].status).to.equal(500);
+        var calledWith = errorSpy.getCall(0).args[0];
+        expect(calledWith.some).to.equal('data');
+        sinon.assert.notCalled(successSpy);
+    });
+
+    it('should only set Content-Type header if none specified', function() {
+        //act
+        rc.get(config);
+        expect(this.requests[0].requestHeaders).to.eql({'Content-Type': 'application/json'});
+    });
+
+    it('should only set Content-Type header if {} specified', function() {
+        config.headers = {};
+
+        //act
+        rc.get(config);
+        expect(this.requests[0].requestHeaders).to.eql({'Content-Type': 'application/json'});
+    });
+
+    it('should set all headers specified', function() {
+
+        config.headers = {'one': 'oneValue',
+            'two': 'twoValue'};
+
+        //act
+        rc.get(config);
+
+        expect(this.requests[0].requestHeaders['one']).to.equal('oneValue');
+        expect(this.requests[0].requestHeaders['two']).to.equal('twoValue');
+    });
+
+    it('should set content type header for post if no header specified', function() {
+        config.headers = null;
+
+        //act
+        rc.post(config);
+        expect(this.requests[0].requestHeaders['Content-Type']).to.contain('application/json');
+    });
+
+    it('should set content type header for post if not specified and retain existing headers', function() {
+        config.headers = {a: 1};
+
+        //act
+        rc.post(config);
+        expect(this.requests[0].requestHeaders['Content-Type']).to.contain('application/json');
+        expect(this.requests[0].requestHeaders['a']).to.equal(1);
+    });
+
+    it('should not override Content-Type header if its specified', function() {
+        config.headers = {'Content-Type': 'application/xml'};
+
+        //act
+        rc.post(config);
+        expect(this.requests[0].requestHeaders['Content-Type']).to.contain('application/xml');
+    });
+
+    it('should use correct defaults if no config provided', function() {
+        var config = {};
+        var client = new RESTClient(config);
+        expect(config.success).to.be.a('function');
+        expect(config.error).to.be.a('function');
+        expect(config.timeout).to.equal(10000);
+        expect(config.async).to.be.true;
+    });
+
+    it('should use set the timeout on the XMLHttpRequest', function() {
+        config.timeout = 50;
+
+        //act
+        rc.ajax(config);
+        expect(this.requests[0].timeout).to.equal(50);
+    });
+
 });
