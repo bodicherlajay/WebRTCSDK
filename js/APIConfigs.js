bfa578d74816767cb62931848d1b93cd51d28bd3
Alex Perkins <halexp@gmail.com>
2014-03-17 16:58:52 -0700

    Fix formatting, added jshint to package.json, remove unneeded comments.

--- js/APIConfigs.js
+++ js/APIConfigs.js
@@ -1,28 +1,28 @@
 /**
-	WebRTC API REST configuration file.
-	All configured methods will be placed in the ATT.WebRTCAPI namespace.
+    WebRTC API REST configuration file.
+    All configured methods will be placed in the ATT.WebRTCAPI namespace.
 */
 var ATT = ATT || {};
 (function (app) {
+    'use strict';
 
-	var APIConfigs = {
-		// Configuration for the DHS endpoint
-		login: {
-			method: 'post',
-			url: 'http://localhost:8080/user/authenticate', // change to the login endpoint on the DHS
-			headers: {'Content-type': 'application/json','Accept' : 'application/json'}
-		},
-		logout: {
-			method: 'delete',
-			url: 'http://localhost:8080/user/logout', // change to the logout endpoint on the DHS
-			headers: {'Content-type': 'application/json','Accept' : 'application/json'}
-		},
-		getSession: {
-			method: 'get',
-			url: 'http://localhost:8080/user/session',
-			headers: {'Content-type': 'application/json','Accept' : 'application/json'}
-		}
-	};
+    var APIConfigs = {
+        login: {
+            method: 'post',
+            url: 'http://localhost:8080/user/authenticate',
+            headers: {'Content-type': 'application/json','Accept' : 'application/json'}
+        },
+        logout: {
+            method: 'delete',
+            url: 'http://localhost:8080/user/logout',
+            headers: {'Content-type': 'application/json','Accept' : 'application/json'}
+        },
+        getSession: {
+            method: 'get',
+            url: 'http://localhost:8080/user/session',
+            headers: {'Content-type': 'application/json','Accept' : 'application/json'}
+        }
+    };
 
-	app.APIConfigs = APIConfigs;
+    app.APIConfigs = APIConfigs;
 }(ATT || {}));
