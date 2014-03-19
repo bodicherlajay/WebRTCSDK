180d3d58d73d15e5e5a8c7c8cf6d841944c51f8e
Toyin Adedoun <toyinonline@me.com>
2014-03-19 14:05:09 -0700

    change DHS calls from port 8080 to 9000

--- js/APIConfigs.js
+++ js/APIConfigs.js
@@ -9,22 +9,22 @@ var ATT = ATT || {};
     var APIConfigs = {
         authenticate: {
             method: 'post',
-            url: 'http://localhost:8080/user/authenticate', // DHS
+            url: 'http://localhost:9000/user/authenticate',
             headers: {'Content-type': 'application/json','Accept' : 'application/json'}
         },
         logout: {
             method: 'delete',
-            url: 'http://localhost:8080/user/logout',
+            url: 'http://localhost:9000/user/logout',
             headers: {'Content-type': 'application/json','Accept' : 'application/json'}
         },
         getBrowserSession: {
             method: 'get',
-            url: 'http://localhost:8080/user/session',
+            url: 'http://localhost:9000/user/session',
             headers: {'Content-type': 'application/json','Accept' : 'application/json'}
         },
         createWebRTCSession: {
             method: 'post',
-            url: 'http://wdev.code-api-att.com:8080/RTC/v1/sessions',   // BF Emulator
+            url: 'http://wdev.code-api-att.com:8080/RTC/v1/sessions',
             headers: {'Content-type': 'application/json','Accept' : 'application/json'}
         }
     };
