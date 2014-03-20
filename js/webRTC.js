d25d0c6c30a79896b561e47b04176aea3e8e5a81
Alex Perkins <halexp@gmail.com>
2014-03-20 16:38:15 -0700

    added functionality/tests to get webrtc session. Renamed karma file (so you'll just need to start with 'karma start').

--- js/webRTC.js
+++ js/webRTC.js
@@ -1,7 +1,7 @@
 /**
 	The WebRTC SDK.
 	API Method namespace is set on the apiNamespace variable.
-	
+
 */
 
 var ATT = ATT || {};
@@ -9,12 +9,15 @@ var ATT = ATT || {};
 (function (app) {
     "use strict";
 	// api operations namespace: (app.WebRTCAPI)
-	var apiNamespace = 'WebRTCAPI',
+	var apiNamespace = 'WebRTC',
 		apiObject = app[apiNamespace] = {};
 
+    /**
+     * Method to initialize the ATT namespace with API methods defined in APIConfigs.js.
+     * @param config
+     */
 	function init(config) {
 		var localConfig = config || {apiConfigs: app.APIConfigs};
-		apiObject = app[apiNamespace] = {};
 
 		// add methods from config
 		if (Object.keys(localConfig.apiConfigs).length > 0) {
@@ -30,7 +33,7 @@ var ATT = ATT || {};
                 o[methodName] = methodConfigs[methodName];
                 addOperation(o);
             }
-		}	
+		}
 	}
 
 	// Add an API method to the ATT namespace
@@ -42,6 +45,11 @@ var ATT = ATT || {};
 		apiObject[methodName] = getConfiguredRESTMethod(methodDescription);
 	}
 
+    /**
+     * Returns function that will call configured REST method.
+     * @param {Object} methodConfig XHR configuration object, e.g. object at APIConfigs['login'].
+     * @returns {Function}
+     */
 	function getConfiguredRESTMethod(methodConfig) {
 
 		return function (data, success, error) {
@@ -54,6 +62,48 @@ var ATT = ATT || {};
             return restClient[methodConfig.method](methodConfig);
 		};
 	}
+    
+    /**
+     * This method will be hit by the login button on the sample app.
+     * Hits authenticate, then createWebRTCSession.  Simply logs the location header
+     * if successfully creates a webrtc session.
+     * 
+     * Todo:  Handle error conditions!
+     * 
+     * @param {Object} data The required login form data from the UI.
+     */
+    function loginAndCreateWebRTCSession (data) {
+        
+        // Call DHS to authenticate, associate user to session.
+        ATT[apiNamespace].authenticate(data, function (responseObject){
+            
+            // get access token, e911 id that is needed to create webrtc session
+            var data = responseObject.getJson(),
+                e911Id = "f81d4fae-7dec-11d0-a765-00a0c91e6bf", // comes from authenticate?
+                
+                dataForCreateWebRTCSession = {
+                    data: {     // Todo: this needs to be configurable in SDK, not hardcoded.
+                        "session" : {
+                            "mediaType":"rtp",
+                            "services": [
+                                "ip_voice_call",
+                                "ip_video_call"
+                            ]
+                        }
+                    },
+                    headers: {
+                        "Authorization": "Bearer " + data.access_token.access_token,    // check this.
+                        "x-e911Id": e911Id
+                    }
+                };
+            
+            // Call BF to create WebRTC Session.
+            ATT[apiNamespace].createWebRTCSession(dataForCreateWebRTCSession, function (responseObject) {
+                console.log(responseObject.getResponseHeader('location'));
+            }, function () {});
+            
+        }, function (){});
+    }
 
 	var utils = {
 		/**
@@ -71,4 +121,5 @@ var ATT = ATT || {};
 	app.utils = utils;
 	app.init = init;
 	app.RESTClient = RESTClient;
+    apiObject.loginAndCreateWebRTCSession = loginAndCreateWebRTCSession;
 }(ATT || {}));
--- test/webRTCTest.js
+++ test/webRTCTest.js
@@ -1,129 +1,237 @@
 /**
-	Tests for top-level ATT WebRTC.
+    Tests for top-level ATT WebRTC.
 */
+
 describe('webRTC', function () {
-	var restClientStub;
-	beforeEach(function () {
-		restClientStub = sinon.stub(RESTClient.prototype, 'ajax', 
-			function(config){
-				config.success({code:'code'})
-			}
-		);
+    var apiNamespace = 'WebRTC',
+        requests,
+        xhr;
+    
+    beforeEach(function () {
 
-	});
+        xhr = sinon.useFakeXMLHttpRequest();
+        requests = [];
 
-	afterEach(function () {
-		restClientStub.restore();
-	});
+        xhr.onCreate = function(xhr) {
+            requests.push(xhr);
+        };
+    });
 
-	it('ATT namespace should exist and contain utils', function () {
-		expect(ATT).to.exist;
-		expect(ATT.utils).to.be.an('object');
-	});
+    afterEach(function () {
+        xhr.restore();
+    });
 
-	describe('init', function(){
-		it('should add api method on ATT namespace', function () {
-			ATT.init({
-			 apiConfigs: {
-					foo: {
-						method: 'get'
-					}
-				}
-			});
-			expect(ATT.WebRTCAPI.foo).is.a('function');
-		});
+    it('ATT namespace should exist and contain utils', function () {
+        expect(ATT).to.exist;
+        expect(ATT.utils).to.be.an('object');
+    });
 
-		it('should call rest client with proper arguments from config', function () {
-			var attConfig = {
-				apiConfigs: {
-					foo: {
-						method: 'get',
-						url: 'http://example.com'
-					}
-				}
-			};
+    describe('init', function(){
+        it('should add api method on ATT namespace', function () {
+            ATT.init({
+             apiConfigs: {
+                    foo: {
+                        method: 'get'
+                    }
+                }
+            });
+            expect(ATT[apiNamespace].foo).is.a('function');
+        });
 
-			ATT.init(attConfig);
-		
-			ATT.WebRTCAPI.foo({}, function(){}, function(){});
-			
-			var arg = restClientStub.getCall(0).args[0];
-			expect(arg.method).to.equal(attConfig.apiConfigs.foo.method);
-			expect(arg.url).to.equal(attConfig.apiConfigs.foo.url);
-		});
+        it('should call rest client with proper arguments from config', function () {
+            var attConfig = {
+                apiConfigs: {
+                    foo: {
+                        method: 'get',
+                        url: 'http://example.com'
+                    }
+                }
+            };
+            
+            var spy = sinon.spy(RESTClient.prototype, 'ajax');
+            ATT.init(attConfig);
+            ATT[apiNamespace].foo({}, function(){}, function(){});
+            
+            var configPassedtoAjax = spy.args[0][0];    // first call, first argument
+            expect(configPassedtoAjax.method).to.equal(attConfig.apiConfigs.foo.method);
+            expect(configPassedtoAjax.url).to.equal(attConfig.apiConfigs.foo.url);
+            
+            spy.restore();
+        });
 
-		it('should call rest client with correct callbacks and data', function () {
-			var attConfig = {
-				apiConfigs: {
-					foo: {
-						method: 'get'
-					}
-				}
-			};
+        it('should call rest client with correct callbacks and data', function () {
+            var attConfig = {
+                apiConfigs: {
+                    foo: {
+                        method: 'get'
+                    }
+                }
+            };
 
-			ATT.init(attConfig);
-			
-			var data = {
-				username: 'username',
-				password: 'password'
-			};
-			
-			var successCB = function(){};
-			var errorCB = function(){};
+            ATT.init(attConfig);
 
-			ATT.WebRTCAPI.foo(data, successCB, errorCB);
-			
-			var arg = restClientStub.getCall(0).args[0];
-			expect(arg.data).eql(data);
-			expect(arg.success).equal(successCB);
-			expect(arg.error).equal(errorCB);
-		});
+            var data = {
+                username: 'username',
+                password: 'password'
+            },
+                successCB = function(){},
+                errorCB = function(){};
+            
+            var spy = sinon.spy(RESTClient.prototype, 'ajax');
+            ATT[apiNamespace].foo(data, successCB, errorCB);
+            
+            var configPassedtoAjax = spy.args[0][0];    // first call, first argument
+            
+            // expects
+            expect(configPassedtoAjax.data).eql(data);
+            expect(configPassedtoAjax.data).eql(data);
+            expect(configPassedtoAjax.success).equal(successCB);
+            expect(configPassedtoAjax.error).equal(errorCB);
+            
+            spy.restore();
+        });
 
-		it('should add api methods during init.', function () {
-			ATT.init({
-				apiConfigs: {
-					foo: {
-						method: 'post'
-					},
-					logout: {
-						method: 'delete'
-					}
-				}
-			});
+        it('should add api methods during init.', function () {
+            ATT.init({
+                apiConfigs: {
+                    foo: {
+                        method: 'post'
+                    },
+                    logout: {
+                        method: 'delete'
+                    }
+                }
+            });
 
-			expect(ATT.WebRTCAPI.foo).is.a('function');
-			expect(ATT.WebRTCAPI.logout).is.a('function');
-		});
+            expect(ATT[apiNamespace].foo).is.a('function');
+            expect(ATT[apiNamespace].logout).is.a('function');
+        });
 
-		it('should use APIConfigs.js if no config passed in', function() {
-			ATT.init();
-			expect(ATT.WebRTCAPI.login).is.a('function');
-		});
-		
-		// Add API methods as you add to the APIConfig.js file.
-		[
-			'login', 
-			'logout',
-			'getSession'
-		].forEach(function(methodName){
-			describe('Function ' + methodName, function () {
-    			it("should exist", function (done) {
-					ATT.init();
-					expect(ATT.WebRTCAPI[methodName]).is.a('function');
-					done();
-    			});
-  			});			
-		});
-	});
+        it('should use APIConfigs.js if no config passed in', function() {
+            ATT.init();
+            expect(ATT[apiNamespace].authenticate).is.a('function');
+        });
 
-	describe('utils', function(){
-		describe('hasWebRTC', function(){
-			it('should return true if navigator.mozGetUserMedia or navigator.webkitGetUserMedia or navigator.getUserMedia is a function',function(){
-				//act
-				var hasWebRTC = ATT.utils.hasWebRTC();
-				expect(hasWebRTC).to.be.true;
-			});
-		});	
-	});
+        // Add API methods as you add to the APIConfig.js file.
+        [
+            'authenticate',
+            'logout',
+            'getBrowserSession',
+            'createWebRTCSession',
+            'loginAndCreateWebRTCSession'
+        ].forEach(function(methodName){
+            describe('Function ' + methodName, function () {
+                it("should exist", function (done) {
+                    ATT.init();
+                    expect(ATT[apiNamespace][methodName]).is.a('function');
+                    done();
+                });
+            });
+        });
+    });
+    
+    describe('utils', function () {
+        it('hasWebRTC should return true if navigator.mozGetUserMedia or navigator.webkitGetUserMedia or navigator.getUserMedia is a function', function (){
+            //act
+            var hasWebRTC = ATT.utils.hasWebRTC();
+            expect(hasWebRTC).to.be.true;
+        });
+    });
+    
+    describe('loginAndCreateWebRTCSession', function () {
 
+        /**
+         * Create session curl call
+         * curl -i -X POST -H "Content-Type: application/json" -H "Accept:application/json" -H "Authorization:Bearer abcd" -H "x-e911Id:f81d4fae-7dec-11d0-a765-00a0c91e6bf" -d @bftest.txt http://wdev.code-api-att.com:8080/RTC/v1/sessions
+         */
+        
+        it('should pass access token and e911id to createWebRTCSession and set appropriate headers/data', function () {
+            
+            // plan of attack:
+            // fake the authorization call response.
+            // spy on createWebRTCSession.
+
+            // spy on createWebRTCSession
+            var createWebRTCSessionSpy = sinon.spy(ATT[apiNamespace], 'createWebRTCSession');
+            
+            ATT[apiNamespace].loginAndCreateWebRTCSession({
+                un: 'un',
+                pw: 'pw'
+            });
+            
+            // response json from authorize call.  check the schema.
+            var responseObject1 = {
+                "access_token": {
+                    "access_token": "abcd"
+                }
+            };
+            
+            // response to authorize
+            requests[0].respond(200, {"Content-Type": "application/json"}, JSON.stringify(responseObject1));
+            var args1 = createWebRTCSessionSpy.args[0];
+            
+            // expect
+            expect(args1[0].headers.Authorization).to.contain(responseObject1.access_token.access_token);
+            expect(args1[0].headers['x-e911Id']).to.exist;
+            
+            // restore
+            createWebRTCSessionSpy.restore();
+        });
+
+
+        it('should pass session description as data object to createWebRTCSession', function () {
+            // plan of attack:
+            // fake the authorization call response.
+            // spy on createWebRTCSession.
+
+            // spy on createWebRTCSession
+            var createWebRTCSessionSpy = sinon.spy(ATT[apiNamespace], 'createWebRTCSession');
+            
+            ATT[apiNamespace].loginAndCreateWebRTCSession({
+                un: 'un',
+                pw: 'pw'
+            });
+
+            // response json from authorize call.  check the schema.
+            var responseObject1 = {
+                "access_token": {
+                    "access_token": "abcd"
+                }
+            };
+
+            // response to authorize
+            requests[0].respond(200, {"Content-Type": "application/json"}, JSON.stringify(responseObject1));
+            var args1 = createWebRTCSessionSpy.args[0];
+
+            // expect
+            // should contain mediaType and services.
+            expect(args1[0].data.session.services).to.be.an('array');
+
+            // restore
+            createWebRTCSessionSpy.restore();
+
+        });
+        
+        
+        it ('should call success callback of createWebRTCSession, happy path', function () {
+            
+            // response json from authorize call.  check the schema.
+            var responseObject1 = {
+                "access_token": {
+                    "access_token": "abcd"
+                }
+                },
+                expectedLocationHeader = "/RTC/v1/sessions/4ba569b5-290d-4f1f-b3af-255731383204";
+
+            ATT[apiNamespace].loginAndCreateWebRTCSession({});
+            
+            // response to authorize
+            requests[0].respond(200, {"Content-Type": "application/json"}, JSON.stringify(responseObject1));
+            
+            // response to createWebRTCSession.  Only passes pack response with no body.
+            requests[1].respond(200, {"Content-Type": "application/json", "location": expectedLocationHeader }, JSON.stringify({}));
+            
+            expect(requests[1].getResponseHeader('location')).to.equal(expectedLocationHeader);
+        });
+    });
 });
