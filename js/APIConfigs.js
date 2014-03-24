31f4d078f788bf40f805ca34e1aa400e2e16c956
Garrick West <garrick@acm.org>
2014-03-24 16:16:46 -0700

    Initial cut at JSDoc comments for the API defaults and configs.

--- js/APIConfigs.js
+++ js/APIConfigs.js
@@ -1,44 +1,91 @@
 /**
-    WebRTC API REST configuration file.
-    All configured methods will be placed in the ATT.WebRTCAPI namespace.
-*/
+  *  WebRTC API REST configuration file.
+  *  All configured methods will be placed in the ATT.WebRTCAPI namespace.
+  *  @namespace WebRTCAPI
+  */
 var ATT = ATT || {};
 
 (function (app) {
     'use strict';
 
+  /**
+   * Property defaults.
+   * @namespace WebRTCAPI.DEFAULTS
+   * @type {object}
+   */
     var DEFAULTS = {
+    /**
+     * Developer Hosted Server Resource url.
+     * @memberof WebRTCAPI.DEFAULTS
+     */
         DHSResource: 'http://localhost:9000',
+    /**
+     * Black Flag Resource url.
+     * @memberof WebRTCAPI.DEFAULTS
+     */
         BFResource: 'http://wdev.code-api-att.com:8080/RTC',
+    /**
+     * Default headers.
+     * @memberof WebRTCAPI.DEFAULTS
+     */
         headers: {'Content-type': 'application/json','Accept' : 'application/json'}
     };
 
+  /**
+   *  Property defaults.
+   * @namespace WebRTCAPI.APIConfigs
+   * @type {object}
+   */
     var APIConfigs = {
+    /**
+     * Authentication to DHSResource
+     * @memberof WebRTCAPI.APIConfigs
+     */
         authenticate: {
             method: 'post',
             url: DEFAULTS.DHSResource + '/user/authenticate',
             headers: DEFAULTS.headers
         },
+    /**
+     * Get access token from DHSResource
+     * @memberof WebRTCAPI.APIConfigs
+     */
         getAccessToken: {
             method: 'get',
             url: DEFAULTS.DHSResource + '/user/token',
             headers: DEFAULTS.headers
         },
+    /**
+     * Logout from DHSResource
+     * @memberof WebRTCAPI.APIConfigs
+     */
         logout: {
             method: 'delete',
             url: DEFAULTS.DHSResource + '/user/logout',
             headers: DEFAULTS.headers
         },
+    /**
+     * Get browser session from DHSResource
+     * @memberof WebRTCAPI.APIConfigs
+     */
         getBrowserSession: {
             method: 'get',
             url: DEFAULTS.DHSResource + '/user/session',
             headers: DEFAULTS.headers
         },
+    /**
+     * Create WebRTC session from BFResource
+     * @memberof WebRTCAPI.APIConfigs
+     */
         createWebRTCSession: {
             method: 'post',
             url: DEFAULTS.BFResource + '/v1/sessions',
             headers: DEFAULTS.headers
         },
+    /**
+     * Get WebRTC events from BFResource
+     * @memberof WebRTCAPI.APIConfigs
+     */
         getEvents: {
             method: 'get',
             url: DEFAULTS.BFResource + '/v1/sessions/{sessionId}/events',
