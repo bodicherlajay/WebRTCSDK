66429a3ac21b9a0e7c0db82163b51b92ae33bd02
Pablo Padilla <pgpb.padilla@gmail.com>
2014-03-25 15:36:21 -0700

    Remove grunt tasks: - watch - jshint (JSLint will be used)

--- Gruntfile.js
+++ Gruntfile.js
@@ -2,6 +2,7 @@ module.exports = function (grunt) {
 
   grunt.initConfig({
     pkg: grunt.file.readJSON('package.json'),
+    // Concatenate all source files into a single JS file.
     concat: {
       options: {
         separator: ';'
@@ -11,38 +12,23 @@ module.exports = function (grunt) {
         dest: 'dist/<%= pkg.name %>.js'
       }
     },
+    // Minify concatenated file.
     uglify: {
       options: {
         banner: '/*! <%= pkg.name %> <%= grunt.template.today("dd-mm-yyyy") %> */\n'
       },
       dist: {
         files: {
-          'dist/<%= pkg.name %>.min.js': ['<%= concat.dist.dest %>']
+          'dist/<%= pkg.name %>_<%= grunt.template.today("dd-mm-yyyy") %>.min.js': ['<%= concat.dist.dest %>']
         }
       }
-    },
-    jshint: {
-      files: ['Gruntfile.js', 'js/**/*.js', 'test/**/*.js'],
-      options: {
-        // options here to override JSHint defaults
-        globals: {
-          jQuery: true,
-          console: true,
-          module: true,
-          document: true
-        }
-      }
-    },
-    watch: {
-      files: ['<%= jshint.files %>']
     }
   });
 
   grunt.loadNpmTasks('grunt-contrib-uglify');
-  grunt.loadNpmTasks('grunt-contrib-jshint');
-  grunt.loadNpmTasks('grunt-contrib-watch');
   grunt.loadNpmTasks('grunt-contrib-concat');
 
-  grunt.registerTask('default', ['jshint', 'concat', 'uglify']);
+  // concatenate first, then minify
+  grunt.registerTask('default', ['concat', 'uglify']);
 
 };
