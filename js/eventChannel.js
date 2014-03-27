d9e8a8e6b8d527785fcccd4c929c178dbe1b9d6c
Toyin Adedoun <toyinonline@me.com>
2014-03-27 10:21:39 -0700

    debugging stuff

--- js/eventChannel.js
+++ js/eventChannel.js
@@ -11,11 +11,10 @@ var ATT = ATT || {};
     * @param {Boolean} useLongPolling Use Long Polling
     */
     function getEventChannel( useLongPolling ) {
-
       // config for setting up Web Socket with BF
       var wsConfig = {
         method: 'post',
-        url: 'http://wdev.code-api-att.com:8080/RTC/v1/sessions/' + ATT.WebRTC.Session.Id + '/websocket',
+        url: 'http://wdev.code-api-att.com:8080/RTC/v1/sessions/' + '3c0112b2-96a7-4c9e-ba63-850bc520fe3e' + '/websocket',
         headers: {
           'Authorization': 'Bearer ' + ATT.WebRTC.Session.accessToken,
           'Content-type': 'application/json',
@@ -24,18 +23,28 @@ var ATT = ATT || {};
         success: function( response ) {
           // grab the location from response headers
           var location = response.getResponseHeader('location');
-          console.log('Connected to websocket: ' + location);
-          // create new WebSocket instance with
-          var ws = new WS(location, function(e) {
-            // parse response
-            var responseEvent = JSON.parse(e.data);
+
+          // if we have a success location
+          if ( location ) {
+            // channelID is channel query string param
+            var channelID = location.split('=')[1];
             // dump to console
-            console.log(responseEvent);
-            // grab the sessionID
-            var sessID = responseEvent.events.eventList[0].eventObject.resourceURL.split('/')[4];
-            // publish sessionID along with responseEvent payload
-            ATT.event.publish( sessID + '.responseEvent', responseEvent );
-          });
+            console.log('CONNECTION CREATED. CHANNEL ID = ' + channelID);
+            // create new WebSocket instance
+            var ws = new WS(location, function(e) {
+              // parse response
+              var responseEvent = JSON.parse(e.data);
+              // dump res & event state to console
+              console.log(responseEvent, responseEvent.events.eventList[0].eventObject.state);
+              // grab the sessionID
+              var sessID = responseEvent.events.eventList[0].eventObject.resourceURL.split('/')[4];
+              // publish sessionID along with responseEvent Object
+              ATT.event.publish( sessID + '.responseEvent', responseEvent );
+            });
+          }
+        },
+        error: function() {
+          console.log('ERROR');
         }
       }
 
