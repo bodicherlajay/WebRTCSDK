3a8c4c998513f95307f6c320a361c7b117e19016
Pablo Padilla <pgpb.padilla@gmail.com>
2014-04-02 23:47:17 -0700

    Removing Karma config files. All Karma configuration is now handled using Grunt.
    $grunt karma:unit
    
    Firefox 28.0.0 (Mac OS X 10.8): Executed 22 of 23 (skipped 1) SUCCESS (0.079 secs / 0.04 secs)
    Chrome 33.0.1750 (Mac OS X 10.8.5): Executed 22 of 23 (skipped 1) SUCCESS (0.083 secs / 0.033 secs)
    TOTAL: 44 SUCCESS

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,4 +1,51 @@
+/*jslint indent:2*/
 module.exports = function (grunt) {
+  'use strict';
+  var karmaConfig = {
+    basePath: '.',
+    frameworks: ['mocha', 'chai', 'sinon'],
+    files: [
+      'js/**/*.js',
+      'test/**/*.js'
+    ],
+    logLevel: 'DEBUG',
+    port: 9876,
+    browsers: ['Firefox', 'Chrome'],
+    captureTimeout: 60000
+  },
+    karmaConfigUnit = {
+      reporters: ['spec'],
+      colors: true,
+      singleRun: false
+    },
+    karmaConfigJenkins = {
+      preprocessors: {
+        'js/**/*.js': 'coverage'
+      },
+      junitReporter: {
+        outputFile: 'out/junit/results.xml'
+      },
+      reporters: ['junit', 'coverage'],
+      coverageReporter: {
+        type : 'cobertura',
+        dir : 'coverage/',
+        file: 'coverage.xml'
+      },
+      colors: false,
+      autoWatch: false,
+      singleRun: true
+    },
+    attrname;
+
+  // little utility to merge options
+  for (attrname in karmaConfig) {
+    if (karmaConfig.hasOwnProperty(attrname)) {
+      // copy all properties from the Global config to the Jenkins config
+      karmaConfigJenkins[attrname] = karmaConfig[attrname];
+      // copy all properties from the Global config to the Unit Testing config
+      karmaConfigUnit[attrname] = karmaConfig[attrname];
+    }
+  }
 
   grunt.initConfig({
     pkg: grunt.file.readJSON('package.json'),
@@ -6,21 +53,20 @@ module.exports = function (grunt) {
     // equivalent to `karma start <config file>
     karma: {
       unit: {
-        configFile: 'karma.conf.js',
+        options: karmaConfigUnit,
         autoWatch: true
       },
-      continuous: {
-        configFile: 'jenkins-karma.config.js',
-        singleRun: true
+      jenkins: {
+        options: karmaConfigJenkins
       }
     },
-    jslint: { 
+    jslint: {
       // lint your project's server code
       server: {
-        src: ['js/**/*.js'],
+        src: ['js/**/*.js', 'test/**/*.js'],
         options: {
-          log: 'jslint.log',
-          checkstyle: 'jslint.xml' // write a checkstyle-XML
+          log: 'out/jslint/jslint.log',
+          checkstyle: 'out/jslint/jslint.xml' // write a checkstyle-XML
         }
       }
     },
@@ -52,5 +98,5 @@ module.exports = function (grunt) {
   grunt.loadNpmTasks('grunt-contrib-uglify');
   grunt.loadNpmTasks('grunt-contrib-concat');
 
-  grunt.registerTask('default', ['jslint', 'karma:continuous', 'concat', 'uglify']);
+  grunt.registerTask('default', ['karma:jenkins', 'jslint', 'concat', 'uglify']);
 };
