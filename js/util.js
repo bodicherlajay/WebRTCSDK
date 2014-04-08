4a3654b21c8e05d6d33ba944f45b97e9b2a0f221
Alex Perkins <halexp@gmail.com>
2014-04-08 09:22:44 -0700

    combined util methods in util module.  Added createNamespace function.

--- js/util.js
+++ js/util.js
@@ -1,35 +1,71 @@
 /*jslint browser: true, devel: true, node: true, debug: true, todo: true, indent: 2, maxlen: 150, nomen:true */
 
-var util = {
-  extend: function (destination, source) {
-    "use strict";
-    var property;
-    for (property in source) {
-      // if the source has `property` as a `direct property`
-      if (source.hasOwnProperty(property)) {
-        // if that property is NOT an `Object`
-        if (!(source[property] instanceof Object)) {
-          // copy the value into the destination object
-          destination[property] = source[property];
-        } else {// `property` IS an `Object`
-          // copy `property` recursively
-          destination[property] = this.extend(source[property]);
+/*jslint browser: true, devel: true, node: true, debug: true, todo: true, indent: 2, maxlen: 150 */
+/*global ATT:true, RESTClient, Env, cmgmt */
+
+if (!ATT) {
+  var ATT = {};
+}
+
+(function (app) {
+  "use strict";
+
+  /**
+   * Check if browser has WebRTC capability.
+   * @return {Boolean}
+   */
+
+  var hasWebRTC =  function () {
+    return typeof navigator.mozGetUserMedia === 'function' ||
+      typeof navigator.webkitGetUserMedia === 'function' ||
+      typeof navigator.getUserMedia === 'function';
+  },
+    extend = function (destination, source) {
+      var property;
+      for (property in source) {
+        // if the source has `property` as a `direct property`
+        if (source.hasOwnProperty(property)) {
+          // if that property is NOT an `Object`
+          if (!(source[property] instanceof Object)) {
+            // copy the value into the destination object
+            destination[property] = source[property];
+          } else {// `property` IS an `Object`
+            // copy `property` recursively
+            destination[property] = this.extend(source[property]);
+          }
         }
       }
-    }
-    return destination;
-  },
+      return destination;
+    },
+    inherits = function (ctor, superCtor) {
+      ctor.super_ = superCtor;
+      ctor.prototype = Object.create(superCtor.prototype, {
+        constructor: {
+          value: ctor,
+          enumerable: false,
+          writable: true,
+          configurable: true
+        }
+      });
+    },
 
-  inherits: function (ctor, superCtor) {
-    "use strict";
-    ctor.super_ = superCtor;
-    ctor.prototype = Object.create(superCtor.prototype, {
-      constructor: {
-        value: ctor,
-        enumerable: false,
-        writable: true,
-        configurable: true
-      }
-    });
-  }
-};
+    createNamespace = function (root, s) {
+      var names = s.split('.'),
+        parent = root,
+        apiObj = parent;
+
+      names.forEach(function (name) {
+        parent[name] = {};
+        apiObj = parent;
+        parent = parent[name];
+      });
+      return apiObj;
+    };
+
+  app.utils = {
+    createNamespace: createNamespace,
+    extend: extend,
+    hasWebRTC: hasWebRTC,
+    inherits: inherits
+  };
+}(ATT));
