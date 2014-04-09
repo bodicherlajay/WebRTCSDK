13215e92110fa33598e5b8789cb80cecb81bcf21
Alex Perkins <halexp@gmail.com>
2014-04-09 08:34:43 -0700

    reformatting for readability.

--- js/eventEnum.js
+++ js/eventEnum.js
@@ -11,36 +11,36 @@ if (!ATT) {
   var module = {};
 
   module.CallStatus = {
-    READY: 0,
-    CALLING: 1,
-    RINGING: 2,
+    READY:      0,
+    CALLING:    1,
+    RINGING:    2,
     INPROGRESS: 3,
-    HOLD: 4,
+    HOLD:       4,
     TRANSITION: 5,
-    WAITING: 6,
-    ENDED: 'ENDED',
-    ERROR: 8
+    WAITING:    6,
+    ENDED:      'ENDED',
+    ERROR:      8
   };
 
   module.RTCEvents = {
-    SESSION_OPEN: "session-open",
-    SESSION_MODIFIED: "session-modified",
-    SESSION_TERMINATED: "session-terminated",
-    INVITATION_RECEIVED: "invitation-received",
-    MODIFICATION_RECEIVED: "mod-received",
-    MODIFICATION_TERMINATED: "mod-terminated",
-    TRANSFER_INITIATED: "transfer-initiated",
-    TRANSFER_TERMINATED: "transfer-terminated",
-    ADD_FAILED: "add-failed",
-    REMOVE_FAILED: "remove-failed",
-    MOVE_TERMINATED: "Move Terminated",
-    UNKNOWN: "Unknown"
+    SESSION_OPEN:             "session-open",
+    SESSION_MODIFIED:         "session-modified",
+    SESSION_TERMINATED:       "session-terminated",
+    INVITATION_RECEIVED:      "invitation-received",
+    MODIFICATION_RECEIVED:    "mod-received",
+    MODIFICATION_TERMINATED:  "mod-terminated",
+    TRANSFER_INITIATED:       "transfer-initiated",
+    TRANSFER_TERMINATED:      "transfer-terminated",
+    ADD_FAILED:               "add-failed",
+    REMOVE_FAILED:            "remove-failed",
+    MOVE_TERMINATED:          "Move Terminated",
+    UNKNOWN:                  "Unknown"
   };
 
   module.MediaTypes = {
-    AUDIO_VIDEO: "audiovideo",
+    AUDIO_VIDEO:      "audiovideo",
     MEDIA_CONFERENCE: "media-conference",
-    MEDIACONFERENCE: "mediaconference"
+    MEDIACONFERENCE:  "mediaconference"
   };
   mainModule.CallStatus = Object.freeze(module.CallStatus);
   mainModule.RTCEvents = Object.freeze(module.RTCEvents);
