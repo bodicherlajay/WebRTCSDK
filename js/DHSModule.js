54bdc395bfed1150ffe6e87c8999fb48746e630d
Yogesh Randhawa <yogesh.randhawa@gmail.com>
2014-04-21 11:04:27 -0700

    delinting

--- js/DHSModule.js
+++ js/DHSModule.js
@@ -141,7 +141,7 @@ if (!Env) {
     // get access token, e911 id that is needed to create webrtc session
     var authenticateResponseData = responseObject.getJson(),
       accessToken = authenticateResponseData.accesstoken ? authenticateResponseData.accesstoken.access_token : null,
-      e911Id =  authenticateResponseData.e911Id ? authenticateResponseData.e911Id.e911Locations.addressIdentifier : null ,
+      e911Id =  authenticateResponseData.e911Id ? authenticateResponseData.e911Id.e911Locations.addressIdentifier : null,
       successcb = function () {};
 
     // if no access token return user data to UI, without webrtc session id
