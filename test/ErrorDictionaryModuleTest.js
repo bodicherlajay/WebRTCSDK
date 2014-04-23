3b75060f6d406acc0b23b0a6dc9bf14301a06470
Pablo Padilla <pgpb.padilla@gmail.com>
2014-04-23 14:42:04 -0700

    ErrorDictionary: Refactor + Unit Tests (+3 squashed commits) Squashed commits: [5b4f5c1] Restructuring Unit Tests for legibility. [d0f0b48] Refactor ErrorDictionaryModule to make it more testable and re-usable. [238f576] Refactor Tests for ErrorDictionary

--- test/ErrorDictionaryModuleTest.js
+++ test/ErrorDictionaryModuleTest.js
@@ -1,41 +1,74 @@
 /*jslint browser: true, devel: true, node: true, debug: true, todo: true, indent: 2, maxlen: 150 */
-/*global ATT:true, errorDictionary:true, describe:true, it: true, expect: true*/
+/*global ATT:true, errorDictionary, describe, it, expect, beforeEach*/
 
 describe('ErrorDictionaryModule', function () {
   "use strict";
-  var errorDictionary = ATT.errorDictionary.getInstance();
-  it('should exist', function () {
-    expect(errorDictionary).to.be.an('object');
+
+  describe('Application Object', function () {
+    it('should have factory method', function () {
+      expect(ATT.createErrorDictionary).to.be.a('function');
+    });
   });
 
-  it('should create new error, format error message', function () {
-    var err = errorDictionary.newError({userErrorCode: '00000',    //5 digit error code
-      operationName: 'Create Session',    //Name of the REST operation
-      httpStatusCode: '400',   //HTTP Status code
-      messageId: 'SVC0002',        //SVC or POL Error
-      helpText: 'One or more of the input parameters are not valid for this operation',         //Help text which can be displayed on UI
-      reasonText: 'invalidinput',       //High level reason text, invalid input, forbidden
-      errorDescription: 'Request payload does not conform as documented', //Error Description
-      moduleID: 'RTC'          //one of the configured module name
-      });
-    expect(err.formatError()).to.equal('RTC-00000-Create Session-400-SVC0002-invalidinput-Request payload does not conform as documented');
-    expect(err.errorId()).to.equal('RTC00000Create Session400SVC0002');
-    expect(errorDictionary.getError(err.errorId()).reasonText).to.equal('invalidinput');
-  });
+  describe('Error Dictionary Object', function () {
+    var errorDictionary,
+      dictionarySpec,
+      errorSpec;
 
-  it('should throw error if module id does not exist', function () {
-    try {
-      var err = errorDictionary.newError({userErrorCode: '00000',    //5 digit error code
+    beforeEach(function () {
+      dictionarySpec = {
+        modules: {
+          M1: 'Module1',
+          M2: 'Module2',
+          M3: 'Module3',
+          RTC: 'RTC Module'
+        }
+      };
+      errorSpec = {
+        userErrorCode: '00000',    //5 digit error code
         operationName: 'Create Session',    //Name of the REST operation
         httpStatusCode: '400',   //HTTP Status code
         messageId: 'SVC0002',        //SVC or POL Error
         helpText: 'One or more of the input parameters are not valid for this operation',         //Help text which can be displayed on UI
         reasonText: 'invalidinput',       //High level reason text, invalid input, forbidden
         errorDescription: 'Request payload does not conform as documented', //Error Description
-        moduleID: 'RTC2'          //one of the configured module name
-        });
-    } catch (e) {
-      expect(e.toString()).to.equal('Error: Invalid Module ID');
-    }
+        moduleID: 'RTC'          //one of the configured module name
+      };
+      // Initialize the dictionary
+      errorDictionary = ATT.createErrorDictionary(dictionarySpec);
+    });
+
+    it('should return valid Error Dictionary', function () {
+      expect(errorDictionary).to.be.an('object');
+    });
+
+    it('should return error object for valid module IDs', function () {
+      // Error spec with a valid module
+      errorSpec.moduleID = 'M1';
+      var err = errorDictionary.createError(errorSpec);
+      expect(err).to.be.an('object');
+    });
+
+    it('should fail (throw error) when creating an error with an invalid Module IDs', function () {
+      errorSpec.moduleID = 'Invalid Module ID';
+      expect(errorDictionary.createError.bind(errorDictionary, errorSpec)).to.throw('Invalid Module ID');
+    });
+
+    it('should return previously created error', function () {
+      var err = errorDictionary.createError(errorSpec);
+      expect(errorDictionary.getError(err.getId())).to.be.an('object');
+    });
+
+    describe('Error Object', function () {
+      it('should generate correct ID', function () {
+        var err = errorDictionary.createError(errorSpec);
+        expect(err.getId()).to.equal('RTC00000Create Session400SVC0002');
+      });
+
+      it('should format error messages properly', function () {
+        var err = errorDictionary.createError(errorSpec);
+        expect(err.formatError()).to.equal('RTC-00000-Create Session-400-SVC0002-invalidinput-Request payload does not conform as documented');
+      });
+    });
   });
 });
