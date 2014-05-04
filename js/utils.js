3388ad5de21888856fd3b8fdc583fc283204840d
smurugu <smurugu@yahoo.com>
2014-05-04 10:51:42 -0700

    Node js needs ATT namespace upfront

--- js/utils.js
+++ js/utils.js
@@ -1,8 +1,11 @@
 /*jslint browser: true, devel: true, node: true, debug: true, todo: true, indent: 2, maxlen: 150*/
 /*global ATT*/
 /** Will export utilities, therefore nothing in here depends on anything else.
-  * Will create `ATT.utils` namespace
-*/
+ * Will create `ATT.utils` namespace
+ */
+if (!ATT) {
+  var ATT = {};
+}
 (function (ATT) {
   'use strict';
 
@@ -11,10 +14,10 @@
    * @return {Boolean}
    */
   var hasWebRTC =  function () {
-    return typeof navigator.mozGetUserMedia === 'function' ||
-      typeof navigator.webkitGetUserMedia === 'function' ||
-      typeof navigator.getUserMedia === 'function';
-  },
+      return typeof navigator.mozGetUserMedia === 'function' ||
+        typeof navigator.webkitGetUserMedia === 'function' ||
+        typeof navigator.getUserMedia === 'function';
+    },
     /**
      * Extends an existing object using deep copy.
      * Note: It will only deep-copy instances of Object.
@@ -22,7 +25,7 @@
      * @param source
      * @returns {*} destination
      */
-    extend = function (destination, source) {
+      extend = function (destination, source) {
       var property;
       for (property in source) {
         // if the source has `property` as a `direct property`
@@ -52,10 +55,10 @@
     },
 
     /**
-      Places namespaces on root object.  s is dot separated string of names adding to root.
-      The namespace created is returned.
-    */
-    createNamespace = function (root, s) {
+     Places namespaces on root object.  s is dot separated string of names adding to root.
+     The namespace created is returned.
+     */
+      createNamespace = function (root, s) {
       var names = s.split('.'),
         parent = root;
 
@@ -64,7 +67,9 @@
         parent = parent[name];
       });
       return parent;
-    };
+    },
+    typeofModule = typeof module,
+    typeofExports = typeof exports;
 
   // create `utils` namespace under `ATT`
   ATT.utils = {
@@ -73,5 +78,13 @@
     extend: extend,
     inherits: inherits
   };
+
+  //exports for nodejs, derived from underscore.js
+  if (typeofExports !== 'undefined') {
+    if (typeofModule !== 'undefined' && module.exports) {
+      exports = module.exports = ATT.utils;
+    }
+    //exports.ATT.utils = ATT.utils;
+  }
 
 }(ATT));
