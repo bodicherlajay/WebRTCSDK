ff76f91739d6196bcabd28f1ab5dfa3568a9671f
Toyin Adedokun <toyinonline@me.com>
2014-05-05 15:48:10 -0700

    unit tests

--- test/eventEnumTest.js
+++ test/eventEnumTest.js
@@ -1,10 +1,25 @@
 /*jslint browser: true, devel: true, node: true, debug: true, todo: true, indent: 2, maxlen: 150*/
-/*global ATT:true, describe:true,it:true,expect:true*/
+/*global ATT:true, describe:true,it:true,expect:true, assert:true*/
 
 //TODO Add more tests
 describe('EventEnums', function () {
   "use strict";
-  it('should contain Event Type CALLING', function () {
-    expect(ATT.CallStatus.CALLING).equals(1);
+
+  it('should contain Needed Enums', function () {
+    assert.typeOf(ATT.UserTypes, 'object');
+    assert.typeOf(ATT.SdkEvents, 'object');
+    assert.typeOf(ATT.SessionEvents, 'object');
+    assert.typeOf(ATT.CallStatus, 'object');
+    assert.typeOf(ATT.RTCCallEvents, 'object');
+    assert.typeOf(ATT.MediaTypes, 'object');
+  });
+
+  it('should have `frozen` Enums', function () {
+    assert.isTrue(Object.isFrozen(ATT.UserTypes));
+    assert.isTrue(Object.isFrozen(ATT.SdkEvents));
+    assert.isTrue(Object.isFrozen(ATT.SessionEvents));
+    assert.isTrue(Object.isFrozen(ATT.CallStatus));
+    assert.isTrue(Object.isFrozen(ATT.RTCCallEvents));
+    assert.isTrue(Object.isFrozen(ATT.MediaTypes));
   });
 });
