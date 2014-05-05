f64b48534c8db56d37be2e290d76d13db2b0e4d5
Toyin Adedokun <toyinonline@me.com>
2014-05-05 10:55:55 -0700

    fix logger and make crockford happy

--- js/CallManagementModule.js
+++ js/CallManagementModule.js
@@ -1,150 +1,147 @@
 /*jslint browser: true, devel: true, node: true, debug: true, todo: true, indent: 2, maxlen: 150*/
 /*global cmgmt:true, Logger:true, ATT:true*/
 
-var logMgr = ATT.logManager.getInstance(), logger;
-logMgr.configureLogger('CallManagementModule', logMgr.loggerType.CONSOLE, logMgr.logLevel.DEBUG);
-logger = logMgr.getLogger('CallManagementModule');
-
 cmgmt = (function () {
   'use strict';
   //Call prototype
-  var module = {},
-    Call = function (from, to, media) {
-      var caller = from, callee = to, mediaType = media, localSDP = null;
-      logger.logTrace('call method started, from: ' + from + ', to: ' + to + ', media type: ' + media);
+  var module = {}, logMgr = ATT.logManager.getInstance(), logger, Call, SessionState,
+    SessionContext, session_context, CreateSession, CreateOutgoingCall, CreateIncomingCall,
+    instance, init;
+  logMgr.configureLogger('CallManagementModule', logMgr.loggerType.CONSOLE, logMgr.logLevel.DEBUG);
+  logger = logMgr.getLogger('CallManagementModule');
+  Call = function (from, to, media) {
+    var caller = from, callee = to, mediaType = media, localSDP = null;
+    logger.logTrace('call method started, from: ' + from + ', to: ' + to + ', media type: ' + media);
 
-      return {
-        caller: function () { return caller; },
-        callee: function () { return callee; },
-        mediaType: function () { return mediaType; },
-        setSDP: function (sdp) {
-          localSDP = sdp;
-        },
-        getSDP: function () {
-          return localSDP;
-        },
-        start: Call.start,
-        hold: Call.hold,
-        resume: Call.resume,
-        mute: Call.mute,
-        unmute: Call.unmute,
-        end: Call.hangup
-      };
-    },
-    //Session state enumeration
-    SessionState = {
-      OUTGOING_CALL : 'Outgoing',
-      INCOMING_CALL : 'Incoming',
-      MOVE_CALL : 'Move Call',
-      HOLD_CALL : 'Hold Call',
-      RESUMED_CALL : 'Resumed Call',
-      TRANSFER_CALL : 'Transfer Call',
-      ENDED_CALL : 'Ended Call',
-      READY: 'Ready', //Ready to accept Outgoing or Incoming call
-      SDK_READY: 'SDK Ready'
-    },
-    //Session context to hold session variables
-    SessionContext = function (token, e9Id, sessionId, state) {
-      var currState = state, callObject = null, event = null, accessToken = token, e911Id = e9Id, currSessionId = sessionId,
-        currentCallId, UICbks = {}, currentCall = null;
-      return {
-        getCurrentCall: function () {
-          return currentCall;
-        },
-        setCurrentCall: function (callObj) {
-          currentCall = callObj;
-        },
-        getAccessToken: function () {
-          return accessToken;
-        },
-        setAccessToken: function (token) {
-          accessToken = token;
-        },
-        getE911Id: function () {
-          return e911Id;
-        },
-        setE911Id: function (id) {
-          e911Id = id;
-        },
-        getSessionId: function () {
-          return currSessionId;
-        },
-        setSessionId: function (id) {
-          currSessionId = id;
-        },
-        getCallState: function () {
-          return currState;
-        },
-        setCallState: function (state) {
-          currState = state;
-        },
-        getCallObject: function () {
-          return callObject;
-        },
-        setCallObject: function (callObj) {
-          callObject = callObj;
-        },
-        getEventObject: function () {
-          return event;
-        },
-        setEventObject: function (eventObject) {
-          event = eventObject;
-        },
-        setUICallbacks: function (callbacks) {
-          UICbks = ATT.utils.extend(UICbks, callbacks);
-        },
-        getUICallbacks: function () {
-          return UICbks;
-        },
-        setCurrentCallId: function (event) {
-          currentCallId = event.split('/')[6] || null;
-        },
-        getCurrentCallId: function () {
-          return currentCallId;
-        }
-      };
-    },
-    session_context,
-
-    CreateSession = function (config) {
-      session_context = new SessionContext(config.token, config.e911Id, config.sessionId, SessionState.READY);
-      logger.logTrace('creating session with id: ' + config.sessionId);
-      session_context.setCallState(SessionState.SDK_READY);
-    },
-
-    CreateOutgoingCall = function (config) {
-      var call = new Call(null, config.to, config.mediaConstraints);
-      session_context.setCallObject(call);
-      session_context.setCallState(SessionState.OUTGOING_CALL);
-      session_context.setUICallbacks(config.success);
-      logger.logTrace('creating outgoing call', 'to: ' + config.to + ', constraints: ' + config.mediaConstraints);
-      ATT.UserMediaService.startCall(config);
-    },
-
-    CreateIncomingCall = function (config) {
-      var event = session_context.getEventObject(),
-        call = new Call(event.caller, null, config.mediaConstraints);
-      session_context.setCallObject(call);
-      session_context.setCallState(SessionState.INCOMING_CALL);
-      session_context.setUICallbacks(config.success);
-      logger.logTrace('creating incoming call', 'caller: ' + event.caller + ', constraints: ' + config.mediaConstraints);
-      ATT.UserMediaService.startCall(config);
-    },
-
-    instance,
-
-    init = function () {
-      logger.logTrace('call management module init');
-      return {
-        getSessionContext: function () {
-          return session_context;
-        },
-        SessionState: SessionState,
-        CreateSession: CreateSession,
-        CreateOutgoingCall: CreateOutgoingCall,
-        CreateIncomingCall: CreateIncomingCall
-      };
+    return {
+      caller: function () { return caller; },
+      callee: function () { return callee; },
+      mediaType: function () { return mediaType; },
+      setSDP: function (sdp) {
+        localSDP = sdp;
+      },
+      getSDP: function () {
+        return localSDP;
+      },
+      start: Call.start,
+      hold: Call.hold,
+      resume: Call.resume,
+      mute: Call.mute,
+      unmute: Call.unmute,
+      end: Call.hangup
     };
+  };
+  //Session state enumeration
+  SessionState = {
+    OUTGOING_CALL : 'Outgoing',
+    INCOMING_CALL : 'Incoming',
+    MOVE_CALL : 'Move Call',
+    HOLD_CALL : 'Hold Call',
+    RESUMED_CALL : 'Resumed Call',
+    TRANSFER_CALL : 'Transfer Call',
+    ENDED_CALL : 'Ended Call',
+    READY: 'Ready', //Ready to accept Outgoing or Incoming call
+    SDK_READY: 'SDK Ready'
+  };
+  //Session context to hold session variables
+  SessionContext = function (token, e9Id, sessionId, state) {
+    var currState = state, callObject = null, event = null, accessToken = token, e911Id = e9Id, currSessionId = sessionId,
+      currentCallId, UICbks = {}, currentCall = null;
+    return {
+      getCurrentCall: function () {
+        return currentCall;
+      },
+      setCurrentCall: function (callObj) {
+        currentCall = callObj;
+      },
+      getAccessToken: function () {
+        return accessToken;
+      },
+      setAccessToken: function (token) {
+        accessToken = token;
+      },
+      getE911Id: function () {
+        return e911Id;
+      },
+      setE911Id: function (id) {
+        e911Id = id;
+      },
+      getSessionId: function () {
+        return currSessionId;
+      },
+      setSessionId: function (id) {
+        currSessionId = id;
+      },
+      getCallState: function () {
+        return currState;
+      },
+      setCallState: function (state) {
+        currState = state;
+      },
+      getCallObject: function () {
+        return callObject;
+      },
+      setCallObject: function (callObj) {
+        callObject = callObj;
+      },
+      getEventObject: function () {
+        return event;
+      },
+      setEventObject: function (eventObject) {
+        event = eventObject;
+      },
+      setUICallbacks: function (callbacks) {
+        UICbks = ATT.utils.extend(UICbks, callbacks);
+      },
+      getUICallbacks: function () {
+        return UICbks;
+      },
+      setCurrentCallId: function (event) {
+        currentCallId = event.split('/')[6] || null;
+      },
+      getCurrentCallId: function () {
+        return currentCallId;
+      }
+    };
+  };
+
+  CreateSession = function (config) {
+    session_context = new SessionContext(config.token, config.e911Id, config.sessionId, SessionState.READY);
+    logger.logTrace('creating session with id: ' + config.sessionId);
+    session_context.setCallState(SessionState.SDK_READY);
+  };
+
+  CreateOutgoingCall = function (config) {
+    var call = new Call(null, config.to, config.mediaConstraints);
+    session_context.setCallObject(call);
+    session_context.setCallState(SessionState.OUTGOING_CALL);
+    session_context.setUICallbacks(config.success);
+    logger.logTrace('creating outgoing call', 'to: ' + config.to + ', constraints: ' + config.mediaConstraints);
+    ATT.UserMediaService.startCall(config);
+  };
+
+  CreateIncomingCall = function (config) {
+    var event = session_context.getEventObject(),
+      call = new Call(event.caller, null, config.mediaConstraints);
+    session_context.setCallObject(call);
+    session_context.setCallState(SessionState.INCOMING_CALL);
+    session_context.setUICallbacks(config.success);
+    logger.logTrace('creating incoming call', 'caller: ' + event.caller + ', constraints: ' + config.mediaConstraints);
+    ATT.UserMediaService.startCall(config);
+  };
+
+  init = function () {
+    logger.logTrace('call management module init');
+    return {
+      getSessionContext: function () {
+        return session_context;
+      },
+      SessionState: SessionState,
+      CreateSession: CreateSession,
+      CreateOutgoingCall: CreateOutgoingCall,
+      CreateIncomingCall: CreateIncomingCall
+    };
+  };
 
   Call.hold = function () {
     if (ATT.PeerConnectionService.peerConnection
