3a25dcf3178e7b7a59c03e0224924a95ef1e25b7
Toyin Adedokun <toyinonline@me.com>
2014-05-10 22:02:32 -0700

    added a couple more tests

--- test/eventDispatcherTest.js
+++ test/eventDispatcherTest.js
@@ -1,10 +1,10 @@
 /*jslint indent:2*/
-/*global assert, ATT, describe, it, afterEach, beforeEach, before, sinon, expect, window*/
+/*global assert, ATT, describe, it, afterEach, beforeEach, before, sinon, expect, console, window*/
 
 describe('Event Dispatcher Tests', function () {
   'use strict';
 
-  var backupAtt, utils = ATT.utils;
+  var backupAtt, utils = ATT.utils, eventRegistry;
   beforeEach(function () {
     backupAtt = window.ATT;
   });
@@ -16,6 +16,28 @@ describe('Event Dispatcher Tests', function () {
   describe('Event registry', function () {
     it('should have a createEventRegistry method', function () {
       assert.isFunction(utils.createEventRegistry);
+    });
+  });
+
+  describe('createEventRegistry method', function () {
+    it('should not create a registry if called without callbacks', function () {
+      var badContext = { getUICallbacks: function () { return; } };
+      eventRegistry = utils.createEventRegistry(badContext);
+      assert.notOk(eventRegistry);
+    });
+
+    it('should create a registry if called with callbacks', function () {
+      var goodContext = {
+        getUICallbacks: function () {
+          return {
+            onCallback: function () {
+              console.log('Hi! My name is callack!');
+            }
+          };
+        }
+      };
+      eventRegistry = utils.createEventRegistry(goodContext);
+      assert.ok(eventRegistry);
     });
   });
 });
