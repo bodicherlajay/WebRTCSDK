7ddae2bc7fbae39c202b8bb4b68114b4ce5af25c
Toyin Adedokun <toyinonline@me.com>
2014-05-13 16:01:10 -0700

    'merging' event create functionality

--- js/RTCEventModule.js
+++ js/RTCEventModule.js
@@ -11,12 +11,15 @@ if (!ATT) {
   var callManager = cmgmt.CallManager.getInstance(),
     module = {},
     instance,
-    interceptEventChannelCallback,
-    setupEventBasedCallbacks,
+    RTCEvent,
+    interceptingEventChannelCallback,
+    subscribeToEvents,
     eventRegistry,
     init = function () {
+      eventRegistry = mainModule.utils.createEventRegistry(callManager.getSessionContext());
       return {
-        setupEventBasedCallbacks: setupEventBasedCallbacks
+        hookupEventsToUICallbacks: subscribeToEvents,
+        createEvent: module.createEvent
       };
     };
 
@@ -40,7 +43,7 @@ if (!ATT) {
   * and triggers UI callbacks
   * @param {Object} event The event object
   */
-  interceptEventChannelCallback = function (event) {
+  interceptingEventChannelCallback = function (event) {
     if (!event) {
       return;
     }
@@ -54,23 +57,51 @@ if (!ATT) {
   };
 
   /**
-    This function subscribes to all events 
+    This function subscribes to all events
     being published by the event channel.
     It hands off the event to interceptingEventChannelCallback()
   */
-  setupEventBasedCallbacks = function () {
-    // get current session context
-    var session = callManager.getSessionContext(),
-      sessionId = session.getSessionId();
-
-    // setup events registry
-    eventRegistry = mainModule.utils.createEventRegistry(session);
+  subscribeToEvents = function () {
+    var sessionId = callManager.getSessionContext().getSessionId();
 
     // unsubscribe first, to avoid double subscription from previous actions
-    mainModule.event.unsubscribe(sessionId + '.responseEvent', interceptEventChannelCallback);
+    mainModule.event.unsubscribe(sessionId + '.responseEvent', interceptingEventChannelCallback);
     // subscribe to published events from event channel
-    mainModule.event.subscribe(sessionId + '.responseEvent', interceptEventChannelCallback);
+    mainModule.event.subscribe(sessionId + '.responseEvent', interceptingEventChannelCallback);
     console.log('Subscribed to events');
+  };
+
+  //Event structure for RTCEvent
+  /** Event Object structure for RTC Event
+   * @memberof ATT.rtc.Phone
+   * @param from Origination party
+   * @param to  Destination party
+   * @param state Call State
+   * @param codec Audio/Video Codec
+   * @param error Error Description
+   * @type {{from: string, to: string, timeStamp: string, state: string, error: string}}
+   */
+  RTCEvent = {
+    from: "",
+    to: "",
+    timeStamp: "",
+    state: "",
+    error: ""
+  };
+
+  module.createEvent = function (from, to, state, codec, error) {
+    if (state.hasOwnProperty(ATT.CallStatus)) {
+      throw new Error("State must be of type ATT.CallStatus");
+    }
+    var evt = Object.create(RTCEvent);
+    evt.from = from;
+    evt.to = to;
+    evt.state = state;
+    evt.error = error;
+    evt.timestamp = new Date();
+    evt.codec = codec;
+    Object.freeze(evt);
+    return evt;
   };
 
   module.getInstance = function () {
