abe37ffb62519058f77c3126650f0166e118a68e
smurugu <smurugu@yahoo.com>
2014-05-13 12:56:00 -0700

    Event structure checkin

--- js/RTCEventModule.js
+++ js/RTCEventModule.js
@@ -11,13 +11,15 @@ if (!ATT) {
   var callManager = cmgmt.CallManager.getInstance(),
     module = {},
     instance,
+    RTCEvent,
     interceptingEventChannelCallback,
     subscribeToEvents,
     eventRegistry,
     init = function () {
       eventRegistry = mainModule.utils.createEventRegistry(callManager.getSessionContext());
       return {
-        hookupEventsToUICallbacks: subscribeToEvents
+        hookupEventsToUICallbacks: subscribeToEvents,
+        createEvent: module.createEvent
       };
     };
 
@@ -55,7 +57,7 @@ if (!ATT) {
   };
 
   /**
-    This function subscribes to all events 
+    This function subscribes to all events
     being published by the event channel.
     It hands off the event to interceptingEventChannelCallback()
   */
@@ -67,6 +69,37 @@ if (!ATT) {
     // subscribe to published events from event channel
     mainModule.event.subscribe(sessionId + '.responseEvent', interceptingEventChannelCallback);
     console.log('Subscribed to events');
+  };
+
+  //Event structure for RTCEvent
+  /** Event Object structure for RTC Event
+   * @memberof ATT.rtc.Phone
+   * @param from Origination party
+   * @param to  Destination party
+   * @param state Call State
+   * @param error Error Description
+   * @type {{from: string, to: string, timeStamp: string, state: string, error: string}}
+   */
+  RTCEvent = {
+    from: "",
+    to: "",
+    timeStamp: "",
+    state: "",
+    error: ""
+  };
+
+  module.createEvent = function (from, to, state, error) {
+    if (state.hasOwnProperty(ATT.CallStatus)) {
+      throw new Error("State must be of type ATT.CallStatus");
+    }
+    var evt = Object.create(RTCEvent);
+    evt.from = from;
+    evt.to = to;
+    evt.state = state;
+    evt.error = error;
+    evt.timestamp = new Date();
+    Object.freeze(evt);
+    return evt;
   };
 
   module.getInstance = function () {
--- test/RTCEventModuleTest.js
+++ test/RTCEventModuleTest.js
@@ -1,7 +1,7 @@
 /*jslint indent:2*/
 /*global assert, ATT, describe, it, afterEach, beforeEach, before, sinon, expect, console, window*/
 
-describe('RTC Event Module Tests', function () {
+describe.only('RTC Event Module Tests', function () {
   'use strict';
 
   var backupAtt;
@@ -17,5 +17,23 @@ describe('RTC Event Module Tests', function () {
     it('should exist', function () {
       assert.ok(ATT.RTCEvent);
     });
+
+    it("should create Event", function () {
+      var event = ATT.RTCEvent.getInstance().createEvent("from", "to", ATT.CallStatus.CALLING, "");
+      assert.ok(event);
+      expect(event.from).equals("from");
+      expect(event.to).equals("to");
+      expect(event.state).equals(ATT.CallStatus.CALLING);
+      assert.isNotNull(event.timeStamp);
+    });
+
+    it("should generate error if call state does not match ATT.CallStatus type", function () {
+      try {
+        ATT.RTCEvent.getInstance().createEvent("from", "to", "", "");
+      } catch (e) {
+        expect(e).equal(new Error("State must be of type ATT.CallStatus"));
+      }
+    });
+
   });
 });
