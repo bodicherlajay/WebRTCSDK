9cf98407cec9582b38b2f5b58cab888a2ca8249e
Yogesh Randhawa <yogesh.randhawa@gmail.com>
2014-05-13 17:24:17 -0700

    Added create error method to error namespace

--- js/error.js
+++ js/error.js
@@ -6,12 +6,13 @@
 
   var errorNamespace = {},
     init,
+    create,
     publish,
     callManager,
     logMgr = ATT.logManager.getInstance(),
     logger;
 
-  logMgr.configureLogger('ERROR', logMgr.loggerType.CONSOLE, logMgr.logLevel.DEBUG);
+  logMgr.configureLogger('ERROR', logMgr.loggerType.CONSOLE, logMgr.logLevel.TRACE);
   logger = logMgr.getLogger('ERROR');
 
   init = function () {
@@ -19,12 +20,82 @@
 
     // create namespace.
     errorNamespace = ATT.utils.createNamespace(app, 'rtc.error');
+    errorNamespace.create = create;
     errorNamespace.publish = publish;
   };
+  
+  create = function (err, operation, moduleId) {
+    logger.logDebug('error.create');
+    logger.logTrace('raw error', err);
 
-  publish = function (error) {
+    var error,
+      errObj;
+
+    operation = operation || 'GeneralOperation';
+    moduleId = moduleId || 'RTC';
+
+    if (typeof err === 'string') { // String Errors
+      error = ATT.errorDictionary.getDefaultError({
+        moduleID: moduleId,
+        operationName: operation,
+        errorDescription: 'Operation ' + operation + ' failed',
+        reasonText: err
+      });
+    } else if (err.isSdkErr) { // Previously thrown SDK Errors
+      error = err;
+    } else if (err.getJson) { // Errors from Network/API
+      errObj = err.getJson();
+      if (errObj.RequestError) {    // Known API errors
+        if (errObj.RequestError.ServiceException) { // API Service Exceptions
+          error = ATT.errorDictionary.getErrorByOpStatus(operation, err.getResponseStatus(), errObj.RequestError.ServiceException.MessageId);
+        } else if (errObj.RequestError.PolicyException) { // API Policy Exceptions
+          error = ATT.errorDictionary.getErrorByOpStatus(operation, err.getResponseStatus(), errObj.RequestError.PolicyException.MessageId);
+        } else if (errObj.RequestError.Exception) { // API Exceptions
+          error = ATT.errorDictionary.getDefaultError({
+            moduleID: moduleId,
+            operationName: operation,
+            httpStatusCode: err.getResponseStatus(),
+            errorDescription: 'Operation ' + operation + ' failed',
+            reasonText: errObj.RequestError.Exception.Text
+          });
+        }
+      } else {
+        if (moduleId === 'DHS') {
+          error = ATT.errorDictionary.getDHSError({ // DHS thrown errors
+            operationName: operation,
+            httpStatusCode: err.getResponseStatus(),
+            reasonText: errObj.error || ''
+          });
+        } else {
+          error = ATT.errorDictionary.getDefaultError({ // Unknown API network errors
+            moduleID: moduleId,
+            operationName: operation,
+            httpStatusCode: err.getResponseStatus(),
+            errorDescription: 'Operation ' + operation + ' failed',
+            reasonText: 'WebRTC operation ' + operation + ' failed due to unknown reason'
+          });
+        }
+      }
+    }
+    if (!error) { // Unknown errors
+      error = ATT.errorDictionary.getMissingError();
+    }
+    return error;
+  };
+
+  publish = function (error, operation, handler) {
     logger.logDebug('error.publish');
-    logger.logError(error);
+
+    if (!error.isSdkErr) {
+      error = create(error, operation);
+    }
+
+    logger.logError(error.formatError());
+    logger.logTrace(error);
+
+    if (typeof handler === 'function') {
+      return handler(error);
+    }
 
     if (!callManager) {
       callManager = cmgmt.CallManager.getInstance();
