f36521d2dfba2cb112c1d732809a373e76a566c3
smurugu <smurugu@yahoo.com>
2014-05-14 11:17:42 -0700

    Add cache-control headers for GetEvents operation

--- js/APIConfigs.js
+++ js/APIConfigs.js
@@ -199,7 +199,12 @@ if (!ATT) {
             }
           },
           timeout: 30000,
-          headers: DEFAULTS.headers
+          headers: {
+            'Cache-Control': 'no-cache',
+            'Pragma': 'no-cache',
+            'Content-Type': 'application/json',
+            'Accept' : 'application/json'
+          }
         },
         /**
         * Start Call via RTCEndpoint
