1fae2ba5c78a3e27bf7e795cf55f944fe4d5f5c6
smurugu <smurugu@yahoo.com>
2014-05-22 01:18:45 -0700

    Event Channel test

--- test/eventChannelTest.js
+++ test/eventChannelTest.js
@@ -6,6 +6,21 @@
  */
 describe('Event Channel', function () {
   'use strict';
+  var resourceManager = Env.resourceManager.getInstance(),
+    requests;
+
+  beforeEach(function () {
+    this.xhr = sinon.useFakeXMLHttpRequest();
+    requests = [];
+
+    this.xhr.onCreate = function (xhr) {
+      requests.push(xhr);
+    };
+  });
+
+  afterEach(function () {
+    this.xhr.restore();
+  });
 
   describe('ATT.utils namespace', function () {
     it('should have factory method to create an eventChannel at ATT.utils.createEventChannel', function () {
@@ -13,121 +28,34 @@ describe('Event Channel', function () {
     });
   });
 
-  describe('Event Channel factory method', function () {
+  it('should call resourceManager.doOperation on `startListening` and change `isListening` flag to true', function () {
+    var channelConfig = {
+      accessToken: 'my token',
+      endpoint: '/events',
+      sessionId: 'sessionId',
+      publisher: {
+        publish: sinon.spy()
+      },
+      publicMethodName: 'getEvents',
+      resourceManager: resourceManager,
+      usesLongPolling: true
+    };
 
-    var channelConfig, eventChannel;
+    var httpConfig = {
+      params: {
+        url: {sessionId: channelConfig.sessionId, endpoint: channelConfig.endpoint},
+        headers: {
+          'Authorization' : 'Bearer ' + channelConfig.accessToken
+        }
+      },
+      success: sinon.spy(), //onSuccess.bind(this, config),
+      error: sinon.spy(), //onError.bind(this, config),
+      ontimeout: sinon.spy() //onTimeOut
+    };
 
-    beforeEach(function () {
-      // template configuration object to initialize the Event Channel
-      channelConfig = {
-        accessToken: 'my token',
-        endpoint: '/events',
-        sessionId: 'sessionId',
-        publisher: {
-          publish: sinon.spy()
-        },
-        publicMethodName: 'publicMethodName',
-        usesLongPolling: true
-      };
-      // Mock for the resource manager
-      channelConfig.resourceManager = {
-        addPublicMethod: function () { return true; }
-      };
-    });
-
-    it('should throw error if the config options are invalid', function () {
-      channelConfig = { invalid: 'invalid options'};
-      expect(ATT.utils.createEventChannel.bind(ATT.utils, channelConfig)).to.throw(Error);
-    });
-    it('should return a valid eventChannel given config options', function () {
-      eventChannel = ATT.utils.createEventChannel(channelConfig);
-      expect(eventChannel).to.be.an('object');
-    });
-    describe('Event Channel Object', function () {
-      var response, messages = {};
-
-      beforeEach(function () {
-        // stub the resourceManager
-        channelConfig.resourceManager.getAPIObject = sinon.spy(function () {
-          return {
-            getEvents: sinon.spy()
-          };
-        });
-        // using a mock response
-        response = {
-          events: {
-            eventList: [{
-              eventObject: {
-                resourceURL: '/v1/sessions/aaa-bbb-ccc/calls/12345'
-              }
-            }]
-          }
-        };
-      });
-
-      it('should have a method: `startListening`', function () {
-        eventChannel = ATT.utils.createEventChannel(channelConfig);
-        expect(eventChannel.startListening).to.be.a('function');
-      });
-
-      xit('should call resourceManager.doOperation on `startListening` and change `isListening` flag to true', function () {
-        channelConfig.resourceManager.doOperation = sinon.spy();
-        eventChannel = ATT.utils.createEventChannel(channelConfig);
-        eventChannel.startListening();
-        expect(channelConfig.resourceManager.doOperation.called).to.equal(true);
-        expect(eventChannel.isListening()).to.equal(true);
-      });
-      xit('should call publisher.publish  given a event (long polling)', function () {
-        channelConfig.resourceManager.doOperation = sinon.spy();
-        channelConfig.usesLongPolling = true;
-        // create the event channel, after this, the event channel will have
-        // a success and an error callback
-        eventChannel = ATT.utils.createEventChannel(channelConfig);
-        eventChannel.startListening = sinon.spy();
-        // call the success callback for this event channel
-        messages.responseText = JSON.stringify(response);
-        //channelConfig.success(messages);
-        expect(channelConfig.publisher.publish.called).to.equal(true);
-        expect(channelConfig.resourceManager.doOperation.called).to.equal(true);
-      });
-      xit('should log error detail if polling fails (long polling)', function () {
-        var error = {message: 'FATAL ERROR!!!'};
-        // fake the console
-        console.log = sinon.spy();
-        channelConfig.usesLongPolling = true;
-        // create the event channel, after this, the event channel will have
-        // a success and an error callback
-        eventChannel = ATT.utils.createEventChannel(channelConfig);
-        channelConfig.error(error);
-        expect(console.log.calledWith('ERROR: Network Error: ' + JSON.stringify(error))).to.equal(true);
-      });
-      xit('should retry polling on timeout (long polling)', function () {
-        channelConfig.resourceManager.doOperation = sinon.spy();
-        channelConfig.usesLongPolling = true;
-        // create the event channel, after this, the event channel will have
-        // a success and an error callback
-        eventChannel = ATT.utils.createEventChannel(channelConfig);
-        channelConfig.ontimeout();
-        expect(channelConfig.resourceManager.doOperation.called).to.equal(true);
-      });
-      it('should continue listening for messages on error or timeout');
-      xit('should create a websockets with the given location', function () {
-        channelConfig.usesLongPolling = false;
-        eventChannel = ATT.utils.createEventChannel(channelConfig);
-        // setup the response
-        response = {
-          getResponseHeader: function () {
-            return 'localhost';
-          }
-        };
-        // mock the WebSocket constructor
-        WebSocket = sinon.spy();
-
-        // call the success callback for this event channel
-        channelConfig.success(response);
-        expect(WebSocket.calledWith('localhost')).to.equal(true);
-      });
-      it('should publish an event given a message on the open socket');
-    });
+    var eventChannel = ATT.utils.createEventChannel(channelConfig);
+    eventChannel.startListening(httpConfig);
+    expect(eventChannel.isListening()).to.equal(true);
   });
+
 });
