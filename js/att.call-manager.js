1cef1a9b3345ffd4316d068c15aa0bc034f0be10
Rakesh Malk <Tamerlin@gmail.com>
2014-05-29 08:26:54 -0700

    Phone number cleaning
    Hooking the phone number cleaning library into the call manager

--- js/att.call-manager.js
+++ js/att.call-manager.js
@@ -201,7 +201,8 @@ cmgmt = (function () {
     logger.logDebug('CreateOutgoingCall');
 
     logger.logInfo('Creating outgoing call');
-    var call = new Call(null, config.to, config.mediaConstraints);
+
+    var call = new Call(null, cleanPhoneNumber(config.to), config.mediaConstraints);
 
     // set call and callbacks in current session
     logger.logInfo('Updating current session for outgoing call');
@@ -220,6 +221,17 @@ cmgmt = (function () {
     ATT.UserMediaService.startCall(config);
   };
 
+  /**
+   *  Removes extra characters from the phone number and formats it for
+   *  clear display
+   */
+  function cleanPhoneNumber(number){
+    var cleaned = ATT.phoneNumber.stringify(number);
+
+    if(cleaned.length < 10){
+      
+    }
+  }
 
   /**
   * Create an Incoming Call
