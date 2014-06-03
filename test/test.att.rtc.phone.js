e85892313cee86df801f674255bfef115dc4b411
Toyin Adedokun <toyinonline@me.com>
2014-06-03 10:55:18 -0700

    fix broken unit tests.

--- test/test.att.rtc.phone.js
+++ test/test.att.rtc.phone.js
@@ -2,7 +2,7 @@
 /*global ATT:true, cmgmt, RESTClient, Env, describe: true, it: true, afterEach: true, beforeEach: true,
  before: true, sinon: true, expect: true, xit: true, URL: true, assert*/
 
-describe.only('webRTC', function () {
+describe('webRTC', function () {
   'use strict';
 
   var resourceManager = Env.resourceManager.getInstance(),
