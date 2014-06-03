07c192a76ff58c6fbd7b911c427d8bb6ea2f2351
Yogesh Randhawa <yogesh.randhawa@gmail.com>
2014-06-02 18:40:58 -0700

    RTC manager

---
+++ js/att.rtc-manager.js
@@ -0,0 +1,77 @@
+/*jslint browser: true, devel: true, node: true, debug: true, todo: true, indent: 2, maxlen: 150*/
+/*global cmgmt:true, Logger:true, ATT:true, Env:true*/
+
+//Dependency: ATT.logManager
+
+
+(function (app) {
+  'use strict';
+
+  var errMgr,
+    session,
+    eventManager,
+    logger = Env.resourceManager.getInstance().getLogger("RTCManager");
+
+  function handleError(operation, errHandler, err) {
+    logger.logDebug('handleError: ' + operation);
+
+    logger.logInfo('There was an error performing operation ' + operation);
+
+    var error = errMgr.create(err, operation);
+
+    if (typeof errHandler === 'function') {
+      errHandler(error);
+    }
+  }
+
+  /**
+  * start a new session
+  * @param {Object} options The options
+  * rtcManager.startSession({
+  *   token: 'abcd'
+  *   e911Id: 'e911Id'
+  * })
+  */
+  function startSession(options) {
+    logger.logDebug('startSession');
+
+    options.factories.createSession({
+      token: options.token,
+      e911Id: options.e911Id,
+      onSessionCreated: function (sessionObj) {
+        session = sessionObj; // store the newly created session
+
+        options.factories.createEventManager({
+          sessionId: session.getSessionId(),
+          onEventManagerCreated: function (evtMgr) {
+            eventManager = evtMgr;
+            console.log(eventManager);
+            options.onSessionStarted(session);
+          },
+          onError: handleError.bind(this, 'CreateEventManager', options.onError)
+        });
+      },
+      onError: handleError.bind(this, 'CreateSession', options.onError),
+      errorManager: errMgr,
+      resourceManager: options.resourceManager
+    });
+  }
+
+  /**
+  * Create a new RTC Manager
+  * @param {Object} options The options
+  * })
+  */
+  function createRTCManager(options) {
+    logger.logDebug('createRTCManager');
+
+    errMgr = options.errorManager;
+    eventManager = options.eventManager;
+
+    return {
+      startSession: startSession
+    };
+  }
+
+  app.factories.createRTCManager = createRTCManager;
+}(ATT || {}));
