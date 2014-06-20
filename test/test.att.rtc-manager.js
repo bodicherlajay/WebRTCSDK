1b2f513703fc466a93d546e45f67b181ba5c8dc9
Yogesh Randhawa <yogesh.randhawa@gmail.com>
2014-06-20 10:09:45 -0700

    removed .only

--- test/test.att.rtc-manager.js
+++ test/test.att.rtc-manager.js
@@ -1,7 +1,7 @@
 /*jslint browser: true, devel: true, node: true, debug: true, todo: true, indent: 2, maxlen: 150 */
 /*global Env, ATT, describe, it, afterEach, beforeEach, before, sinon, expect, assert, xit, URL*/
 
-describe.only('RTC Manager', function () {
+describe('RTC Manager', function () {
   'use strict';
 
   var factories,
