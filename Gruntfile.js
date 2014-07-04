eee858474d5b4300d5a851548151d4148c29b5a3
Pablo Padilla <pgpb.padilla@gmail.com>
2014-07-04 14:41:48 -0700

    Setup concatenation+minification+gzip compression for core sdk. Unit Test pass even after concatenation :)
    Remove configuration to test after compression, it doesn't make any sense, duh! (+6 squashed commits)
    Squashed commits:
    [526e380] Use auto name for compressed file
    [2bbee3c] Enable Gzip compression of static content.
    [b6b1d59] setup compression for minified file
    [69fdebb] grunt setup for concatenation + minification + compression
    [76684b3] fix uses of logManager
    [7362a88] grunt setup for testing concatenated file

--- Gruntfile.js
+++ Gruntfile.js
@@ -3,14 +3,28 @@
 module.exports = function (grunt) {
   'use strict';
 
-  var karmaConfig = {
-    basePath: '.',
-    frameworks: ['mocha', 'chai', 'sinon'],
-    files: [
-      {
-        pattern: 'fixtures/**/*.html',
-        included: true
-      },
+  var unitTestFiles = [
+      // 'test/test.att.utils.sdk-error-store.js',
+      // 'test/test.att.config.api.js',
+      'test/test.att.resource-manager.js',
+      'test/test.att.enum.js',
+      'test/test.att.utils.error-dictionary.js',
+      'test/test.att.event-manager.js',
+      'test/test.att.rtc-manager.js',
+      'test/test.att.call.js',
+      'test/test.att.session.js',
+      'test/test.att.utils.event-channel.js',
+      // 'test/test.att.peer-connection-service.js',
+      // 'test/test.att.rtc.event.js',
+      // 'test/test.att.signaling.service.js',
+      // 'test/test.att.utils.sdp-filter.js',
+      // 'test/test.att.user-media-service.js',
+      'test/test.att.rtc.phone.js'
+      // 'test/test.att.rtc.dhs.js',
+      // 'test/test.att.main.js',
+      // 'test/test.att.phonenumber.js'
+    ],
+    srcFiles = [
       'js/att.init.js',
       'lib/vendor/SDPParser.js',
       'lib/js-shared/js/*.js',
@@ -34,38 +48,21 @@ module.exports = function (grunt) {
       'js/att.peer-connection-service.js',
       'js/att.call.js',
       'js/att.session.js',
-//      'js/eventDispatcher.js',
-//      'js/att.rtc.dhs.js',
+      'js/eventDispatcher.js',
+      'js/att.rtc.dhs.js',
       'js/att.rtc.phone.js',
-//      'js/att.main.js',
-
-      // TESTS
-
-//      'test/test.att.utils.sdk-error-store.js',
-//      'test/test.att.config.api.js',
-      'test/test.att.resource-manager.js',
-//      'test/test.att.enum.js',
-//      'test/test.att.utils.error-dictionary.js',
-      'test/test.att.event-manager.js',
-      'test/test.att.rtc-manager.js',
-      'test/test.att.call.js',
-      'test/test.att.session.js',
-      'test/test.att.utils.event-channel.js',
-//      'test/test.att.peer-connection-service.js',
-//      'test/test.att.rtc.event.js',
-//      'test/test.att.signaling.service.js',
-//      'test/test.att.utils.sdp-filter.js',
-//      'test/test.att.user-media-service.js',
-      'test/test.att.rtc.phone.js'
-//      'test/test.att.rtc.dhs.js',
-//      'test/test.att.main.js',
-//      'test/test.att.phonenumber.js'
+      'js/att.main.js'
     ],
-    logLevel: 'INFO',
-    port: 9876,
-    browsers: ['Chrome'],
-    captureTimeout: 60000
-  },
+    allFiles = srcFiles.concat(unitTestFiles),
+    karmaConfig = {
+      basePath: '.',
+      frameworks: ['mocha', 'chai', 'sinon'],
+      files : allFiles,
+      logLevel: 'INFO',
+      port: 9876,
+      browsers: ['Chrome'],
+      captureTimeout: 60000
+    },
     karmaConfigCoverage = {
       preprocessors: {
         'js/**/*.js': 'coverage'
@@ -85,6 +82,7 @@ module.exports = function (grunt) {
       singleRun: false,
       usePolling: true  // This is required on linux/mac. See bug: https://github.com/karma-runner/karma/issues/895
     },
+    karmaConfigConcat = karmaConfigUnit,
     karmaConfigJenkins = {
       preprocessors: {
         'js/**/*.js': 'coverage'
@@ -116,15 +114,12 @@ module.exports = function (grunt) {
     }
   }
 
+  // Karma tests for concatenated single file
+  karmaConfigConcat.files = ([ 'dist/<%= pkg.name %>.js' ]).concat(unitTestFiles);
+
   grunt.initConfig({
     pkg: grunt.file.readJSON('package.json'),
-    // you can run `grunt karma` to execute the config file specified
-    // equivalent to `karma start <config file>
-    karma: {
-      unit: { options: karmaConfigUnit },
-      jenkins: { options: karmaConfigJenkins },
-      coverage: { options: karmaConfigCoverage }
-    },
+
     jslint: {
       // lint your project's server code
       server: {
@@ -143,12 +138,60 @@ module.exports = function (grunt) {
           destination: 'doc'
         }
       }
+    },
+    // Concatenate all source files into a single JS file.
+    concat: {
+      options: {
+        separator: ';'
+      },
+      dist: {
+        src: srcFiles,
+        dest: 'dist/<%= pkg.name %>.js'
+      }
+    },
+    // Minify concatenated file.
+    uglify: {
+      options: {
+        banner: '/*! <%= pkg.name %>_<%= grunt.template.today("dd-mm-yyyy") %> */\n'
+      },
+      dist: {
+        files: {
+          // IN = concat:dist:dest, OUT = dis/pkg.name.min.js
+          'dist/<%= pkg.name %>.min.js': ['<%= concat.dist.dest %>']
+        }
+      }
+    },
+    compress: {
+      main: {
+        options: {
+          mode: 'gzip'
+        },
+        files: [
+          {
+            expand: true,
+            src: ['dist/<%= pkg.name %>.min.js']
+          }
+        ]
+      }
+    },
+    // you can run `grunt karma` to execute the config file specified
+    // equivalent to `karma start <config file>
+    karma: {
+      unit: { options: karmaConfigUnit },
+      concat: { options: karmaConfigConcat },
+      jenkins: { options: karmaConfigJenkins },
+      coverage: { options: karmaConfigCoverage }
     }
   });
 
   grunt.loadNpmTasks('grunt-karma');
   grunt.loadNpmTasks('grunt-jslint');
   grunt.loadNpmTasks('grunt-jsdoc');
+  grunt.loadNpmTasks('grunt-contrib-uglify');
+  grunt.loadNpmTasks('grunt-contrib-concat');
+  grunt.loadNpmTasks('grunt-contrib-compress');
 
   grunt.registerTask('default', ['karma:jenkins', 'jslint']);
+  grunt.registerTask('test:concat', ['concat', 'karma:concat']);
+  grunt.registerTask('release', ['concat', 'uglify', 'compress']);
 };
