c85b343ef3b8da51387914f70a878778442ae04a
arun3528 <arun3528@gmail.com>
2014-07-28 17:42:10 -0700

    adding tests for PeerConnection

--- js/att.peer-connection.js
+++ js/att.peer-connection.js
@@ -1,9 +1,11 @@
 /*jslint browser: true, devel: true, node: true, debug: true, todo: true, indent: 2, maxlen: 150 */
-/*global ATT*/
+/*global ATT, RTCPeerConnection*/
 (function () {
   'use strict';
 
-  function createPeerConnection() {
+  function createPeerConnection(options) {
+
+    var pc;
 
     function setRemoteDescription() {
 
@@ -11,6 +13,24 @@
 
     function setLocalDescription(sdp) {
 
+    }
+
+    if (undefined === options) {
+      throw new Error('Invalid options.');
+    }
+    if ('function' !== typeof options.onPCReady) {
+      throw new Error('Invalid `onPCReady` callback.');
+    }
+    if ('function' !== typeof options.onError) {
+      throw new Error('Invalid `onError` callback.');
+    }
+
+    try {
+      pc = new RTCPeerConnection();
+
+      options.onPCReady();
+    } catch (error) {
+      options.onError(new Error('Failed to create PeerConnection.'));
     }
 
     return {
