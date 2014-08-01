fc138754e9dcc7303abaf7092e032fe279670885
arun3528 <arun3528@gmail.com>
2014-08-01 11:29:40 -0700

    fixes for rtcManager.connectConference

--- js/att.rtc-manager.js
+++ js/att.rtc-manager.js
@@ -333,18 +333,26 @@
     }
 
     function connectConference(options) {
-      var config = {
+      var responseData, config;
+
+        config = {
         data: {
           conferenceModifications: {
             sdp: options.localSdp
           }
         },
         success: function (response) {
+          if (response.getResponseHeader('x-conference-action') === 'call-answer') {
+            responseData = {
+              state: response.getResponseHeader('x-conference-action')
+            };
+          } else {
+            responseData = {
+              id: response.getResponseHeader('Location').split('/')[6],
+              state: response.getResponseHeader('x-state')
+            };
+          }
 
-          var responseData = {
-            id: response.getResponseHeader('Location').split('/')[6],
-            state: response.getResponseHeader('x-state')
-          };
           options.onSuccess(responseData);
         },
         error: function (error) {
