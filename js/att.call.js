f0b91c7f05428477fd4c61f5a68aed6d94e15ebe
Toyin Adedokun <toyinonline@me.com>
2014-08-01 22:19:41 -0700

    bug fix on getting the remoteSdp

--- js/att.call.js
+++ js/att.call.js
@@ -387,7 +387,7 @@
           sessionId: sessionInfo.sessionId,
           token: sessionInfo.token,
           onSuccess: function () {
-            logger.logInfo('Canceled successfully!');
+            logger.logInfo('Canceled successfully.');
 
             rtcManager.resetPeerConnection();
           },
@@ -408,11 +408,10 @@
           callId: id,
           breed: thisCall.breed(),
           onSuccess: function () {
-            logger.logInfo('Successfully hungup the current call');
+            logger.logInfo('Successfully disconnected.');
           },
           onError: function (error) {
             logger.logError(error);
-
             emitter.publish('error', {
               error: error
             });
