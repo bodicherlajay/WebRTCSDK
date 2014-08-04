956c175976f358e78c8af11dd28fae9de44437a4
arun3528 <arun3528@gmail.com>
2014-08-04 12:27:47 -0700

    startConference : ---Added more test case for new error code    jsdoc ,rally test case , logging

--- js/att.rtc.phone.js
+++ js/att.rtc.phone.js
@@ -1271,20 +1271,35 @@
       try {
         if (undefined === options
             || 0 === Object.keys(options).length) {
+          logger.logError(' parameters not found');
           publishError('18000');
           return;
         }
+        if (undefined === session || null === session.getId()) {
+          logger.logError('no session to start  conference');
+          publishError('18007');
+          return;
+        }
+        if (session.currentCall !== null && session.currentCall.breed() === 'conference') {
+          logger.logError('Please End your current Conference');
+          publishError('18006');
+          return;
+        }
+
         if (undefined === options.localMedia) {
+          logger.logError('localmedia not passed');
           publishError('18001');
           return;
         }
         if (undefined === options.remoteMedia) {
+          logger.logError('remotemedia not passed');
           publishError('18002');
           return;
         }
         if ((undefined === options.mediaType)
             || ('audio' !== options.mediaType
             && 'video' !== options.mediaType)) {
+          logger.logError('mediatype not passed');
           publishError('18003');
           return;
         }
@@ -1294,10 +1309,12 @@
         conference = session.createCall(options);
 
         conference.on('error', function (data) {
+          logger.logInfo('published error to user om start conference');
           emitter.publish('error', data);
         });
 
         conference.on('connected', function (data) {
+          logger.logInfo('connected conference event published to UI');
           emitter.publish('conference-connected', data);
         });
 
@@ -1310,15 +1327,18 @@
             conference.addStream(userMedia.localStream);
             conference.connect();
           },
-          onMediaEstablished: function () {
+          onMediaEstablished: function (data) {
             logger.logInfo('onMediaEstablished');
-            logger.logInfo('Media Established');
+            emitter.publish('media-established', data);
           },
           onUserMediaError: function (error) {
+            logger.logError('getUserMedia Failed ');
             publishError('18004', error);
           }
         });
       } catch (err) {
+
+        logger.logError('error while start conference , check logs');
         publishError('18005', err);
       }
 
