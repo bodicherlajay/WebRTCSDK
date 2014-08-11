37f31d24394b9395c23d1ed2575b6eb7744aad46
Yogesh Randhawa <yogesh.randhawa@gmail.com>
2014-08-11 13:30:06 -0700

    removing duplicate definition

--- js/att.rtc.phone.js
+++ js/att.rtc.phone.js
@@ -1803,8 +1803,6 @@
     var instance,
       logger = logManager.addLoggerForModule('rtc');
 
-    ATT.private.pcv = 2;
-
     return {
       /**
       * @summary Get the current instance of Phone.
