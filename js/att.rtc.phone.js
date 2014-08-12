fb8b0acdaa7ff7a4e48ba38dd33f9be37de76040
Hamdi <hamdi-mac@hamdis-mbp.lan>
2014-08-12 14:05:45 -0700

    removed duplicate ATT.private.pcv

--- js/att.rtc.phone.js
+++ js/att.rtc.phone.js
@@ -1798,8 +1798,6 @@
     var instance,
       logger = logManager.addLoggerForModule('rtc');
 
-    ATT.private.pcv = 1;
-
     return {
       /**
       * @summary Get the current instance of Phone.
