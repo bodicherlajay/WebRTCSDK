f6bbd00dcb3979209735960752dbd278bcf602aa
Garrick West <garrick@acm.org>
2014-08-22 14:18:16 -0700

    Improved JSLint Hit List

--- lint-hitlist.sh
+++ lint-hitlist.sh
@@ -1,2 +1,8 @@
 #!/bin/bash
-grunt jslint --force | grep FAIL | sort -k2 -n -r --field-separator=\(
+TIME_NOW=$(date "+%c")
+echo "# ......-=(The JSLint Hit List)=-......."
+echo "## Defending The Faith of Crockford since 2014"
+echo "#### The worst offenders as of $TIME_NOW"
+echo "Total number of JSLint violations in (parenthesis)"
+echo " "
+grunt jslint --no-color --force | sed 's/^/\* /' | grep FAIL | sort -k2 -n -r --field-separator=\(
